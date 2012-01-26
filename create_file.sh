071c00fb3ab895134bb7e6f1b8a41833e849c0fb
Da Zheng <zhengda1936@gmail.com>
2012-01-26 11:58:20 -0500

    create tmpfs filesystem.

--- create_file.sh
+++ create_file.sh
@@ -9,11 +9,13 @@ umount /mnt/ram
 if [ $1 = "ext4" ]; then
 	mkfs.ext4 -O extent /dev/ram0
 	mount -o dioread_nolock /dev/ram0 /mnt/ram
+	chmod 444 /mnt/ram/test
 elif [ $1 = "xfs" ]; then
 	mkfs.xfs -f /dev/ram0
 	mount /dev/ram0 /mnt/ram
+elif [ $1 = "tmpfs" ]; then
+	mount -t tmpfs -o size=5G tmpfs /mnt/tmp
 fi
 
 ./create_file /mnt/ram/test 4G
-chmod 444 /mnt/ram/test
 echo 3 > /proc/sys/vm/drop_caches
