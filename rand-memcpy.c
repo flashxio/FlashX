98e415d167cd40b1bdcb319734a3ad4d3dc34bd0
Da Zheng <zhengda1936@gmail.com>
2012-02-13 16:08:30 -0500

    the implementation of memory copy

---
+++ rand-memcpy.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <string.h>
+
+#define NUM_THREADS 8
+#define PAGE_SIZE 4096
+#define ENTRY_SIZE PAGE_SIZE
+#define ARRAY_SIZE 1073741824
+
+off_t *offset;
+unsigned int nentries;
+int nthreads;
+struct timeval global_start;
+char *array;
+char *dst_arr;
+
+void permute_offset(off_t *offset, int num)
+{
+	int i;
+	for (i = num - 1; i >= 1; i--) {
+		int j = random() % i;
+		off_t tmp = offset[j];
+		offset[j] = offset[i];
+		offset[i] = tmp;
+	}
+}
+
+float time_diff(struct timeval time1, struct timeval time2)
+{
+	return time2.tv_sec - time1.tv_sec
+			+ ((float)(time2.tv_usec - time1.tv_usec))/1000000;
+}
+
+void rand_read(void *arg)
+{
+	int fd;
+	ssize_t ret;
+	int i, j, start_i, end_i;
+	ssize_t read_bytes = 0;
+	struct timeval start_time, end_time;
+
+	start_i = (long) arg;
+	end_i = start_i + nentries / nthreads;
+	gettimeofday(&start_time, NULL);
+	for (j = 0; j < 8; j++) {
+		for (i = start_i; i < end_i; i++) {
+			memcpy(dst_arr + offset[i], array + offset[i], ENTRY_SIZE);
+			read_bytes += ENTRY_SIZE;
+		}
+	}
+	gettimeofday(&end_time, NULL);
+	printf("read %ld bytes, start at %f seconds, takes %f seconds\n",
+			read_bytes, time_diff(global_start, start_time),
+			time_diff(start_time, end_time));
+	
+	pthread_exit((void *) read_bytes);
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	int i;
+	struct timeval start_time, end_time;
+	ssize_t read_bytes = 0;
+	pthread_t threads[NUM_THREADS];
+	/* the number of entries the array can contain. */
+	int arr_nentries = ARRAY_SIZE / ENTRY_SIZE;
+
+	if (argc != 3) {
+		fprintf(stderr, "read num_entries num_threads\n");
+		exit(1);
+	}
+
+	nentries = atoi(argv[1]);
+	offset = malloc(sizeof(*offset) * nentries);
+	for(i = 0; i < nentries; i++) {
+		offset[i] = (random() % arr_nentries) * ENTRY_SIZE;
+		if (offset[i] < 0) {
+			printf("offset[%d]: %ld\n", i, offset[i]);
+			exit(1);
+		}
+	}
+//	permute_offset(offset, nentries);
+
+	array = malloc(ARRAY_SIZE);
+	/* we need to avoid the cost of page fault. */
+	for (i = 0; i < ARRAY_SIZE; i += PAGE_SIZE)
+		array[i] = 0;
+	dst_arr = malloc(ARRAY_SIZE);
+	/* we need to avoid the cost of page fault. */
+	for (i = 0; i < ARRAY_SIZE; i += PAGE_SIZE)
+		dst_arr[i] = 0;
+
+	nthreads = atoi(argv[2]);
+	if (nthreads > NUM_THREADS) {
+		fprintf(stderr, "too many threads\n");
+		exit(1);
+	}
+
+	ret = setpriority(PRIO_PROCESS, getpid(), -20);
+	if (ret < 0) {
+		perror("setpriority");
+		exit(1);
+	}
+
+	gettimeofday(&start_time, NULL);
+	global_start = start_time;
+	for (i = 0; i < nthreads; i++) {
+		ret = pthread_create(&threads[i], NULL,
+				rand_read, (void *) (nentries / nthreads * i));
+		if (ret) {
+			perror("pthread_create");
+			exit(1);
+		}
+	}
+
+	for (i = 0; i < nthreads; i++) {
+		ssize_t size;
+		ret = pthread_join(threads[i], (void **) &size);
+		if (ret) {
+			perror("pthread_join");
+			exit(1);
+		}
+		read_bytes += size;
+	}
+	gettimeofday(&end_time, NULL);
+	printf("read %ld bytes, takes %f seconds\n",
+			read_bytes, end_time.tv_sec - start_time.tv_sec
+			+ ((float)(end_time.tv_usec - start_time.tv_usec))/1000000);
+}
