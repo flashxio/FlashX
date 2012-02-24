056ddb5912fa8d42c56867462b9bb40a0620b8bf
Da Zheng <zhengda1936@gmail.com>
2012-02-23 19:32:13 -0500

    extend create_file.sh

--- create_file.sh
+++ create_file.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
 if [ $# -eq 0 ]; then
-	echo "specify the filesystem"
+	echo "create_file filesystem size ram node_id"
 	exit 1
 fi
 
-umount /mnt/ram
+size=$2
+ram=$3
+node_id=$4
+
+umount /mnt/$ram
+mkdir -p /mnt/$ram
 if [ $1 = "ext4" ]; then
-	mkfs.ext4 -O extent /dev/ram0
-	mount -o dioread_nolock /dev/ram0 /mnt/ram
-	chmod 444 /mnt/ram/test
+	mkfs.ext4 -O extent /dev/$ram
+	mount -o dioread_nolock /dev/$ram /mnt/$ram
+	chmod 444 /mnt/$ram/test
 elif [ $1 = "xfs" ]; then
-	mkfs.xfs -f /dev/ram0
-	mount /dev/ram0 /mnt/ram
+	mkfs.xfs -f /dev/$ram
+	mount /dev/$ram /mnt/$ram
 elif [ $1 = "tmpfs" ]; then
 	mount -t tmpfs -o size=5G tmpfs /mnt/tmp
 fi
 
-./create_file /mnt/ram/test 4G
+./create_file /mnt/$ram/test $size $node_id
 echo 3 > /proc/sys/vm/drop_caches
