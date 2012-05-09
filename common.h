03ea25b990e26cf24ade6cf9bc04d1b47639d962
Da Zheng <zhengda1936@gmail.com>
2012-05-09 13:14:40 -0400

    add common.h

---
+++ common.h
@@ -0,0 +1,18 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+enum {
+	READ,
+	WRITE
+};
+
+extern int nthreads;
+extern struct timeval global_start;
+
+inline float time_diff(struct timeval time1, struct timeval time2)
+{
+	return time2.tv_sec - time1.tv_sec
+			+ ((float)(time2.tv_usec - time1.tv_usec))/1000000;
+}
+
+#endif
