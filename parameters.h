0278cada2819d378b796912d513a8c3f9661a728
Da Zheng <zhengda1936@gmail.com>
2012-11-29 01:46:52 -0500

    Use more reasonable parameters.

--- parameters.h
+++ parameters.h
@@ -3,10 +3,10 @@
 
 #define USE_GCLOCK
 
-const int IO_QUEUE_SIZE = 1024 * 1024;
-const int MAX_FETCH_REQS = 100;
-const int MSG_SEND_BUF_SIZE = 100;
-
 const int AIO_DEPTH_PER_FILE = 32;
+
+const int IO_QUEUE_SIZE = AIO_DEPTH_PER_FILE * 5;
+const int MAX_FETCH_REQS = AIO_DEPTH_PER_FILE;
+const int MSG_SEND_BUF_SIZE = 10;
 
 #endif
