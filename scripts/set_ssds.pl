f2ef4437b3abde2f2bf169207f4450a71adb7806
Da Zheng <zhengda1936@gmail.com>
2013-03-30 15:23:41 -0400

    set up 16 SSDs.

--- scripts/set_ssds.pl
+++ scripts/set_ssds.pl
@@ -2,10 +2,9 @@
 
 use strict;
 
-my @dev_names=("b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p");
-my $file_size = "20G";
+my @dev_names=("b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q");
 
-for (my $i = 1; $i <= 15; $i++) {
+for (my $i = 1; $i <= 16; $i++) {
 	my $dev_idx = $i - 1;
 	my $dev_file = "/dev/sd$dev_names[$dev_idx]1";
 	my $dir = "/mnt/ssd${i}";
