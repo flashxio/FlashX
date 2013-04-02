0f6454fa196209eda5d82ddd4f8cdc0d54c760e6
Da Zheng <zhengda1936@gmail.com>
2013-04-02 13:33:17 -0400

    Fix a minor bug in workload generator.

--- workload.cpp
+++ workload.cpp
@@ -3,7 +3,7 @@
 #include "container.cpp"
 
 int workload_gen::default_entry_size = PAGE_SIZE;
-int workload_gen::default_access_method = READ;
+int workload_gen::default_access_method = -1;
 
 off_t cache_hit_defined_workload::next_offset()
 {
