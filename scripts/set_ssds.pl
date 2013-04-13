e02e274bf9a3c2ec92141efddbecdc57a20e6535
Da Zheng <zhengda1936@gmail.com>
2013-04-12 23:26:53 -0400

    Update set_ssds.pl.

--- scripts/set_ssds.pl
+++ scripts/set_ssds.pl
@@ -11,7 +11,7 @@ for (my $i = 1; $i <= 16; $i++) {
 	print "dev: $dev_file, dir: $dir\n" ;
 	system("mkdir -p $dir");
 	system("mount $dev_file $dir");
-	system("chown zhengda.zhengda $dir/test");
+	system("chown -R zhengda.zhengda $dir");
 	system("echo noop > /sys/block/sd$dev_names[$dev_idx]/queue/scheduler");
 	system("cat /sys/block/sd$dev_names[$dev_idx]/queue/scheduler");
 }
