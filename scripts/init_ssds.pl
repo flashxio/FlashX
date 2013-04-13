ac098344e9c37539a1eb6f5b69bd9e34aa4c7ffb
Da Zheng <zhengda1936@gmail.com>
2013-04-12 23:24:36 -0400

    update init_ssds.pl.

--- scripts/init_ssds.pl
+++ scripts/init_ssds.pl
@@ -2,10 +2,10 @@
 
 use strict;
 
-my @dev_names=("b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p");
-my $file_size = "20G";
+my @dev_names=("b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q");
+my $file_size = "40G";
 
-for (my $i = 1; $i <= 15; $i++) {
+for (my $i = 1; $i <= 16; $i++) {
 	my $dev_idx = $i - 1;
 	my $dev_file = "/dev/sd$dev_names[$dev_idx]1";
 	my $dir = "/mnt/ssd${i}";
@@ -13,7 +13,15 @@ for (my $i = 1; $i <= 15; $i++) {
 	system("mkfs.xfs $dev_file");
 	system("mkdir -p $dir");
 	system("mount $dev_file $dir");
-	system("tools/create_file $dir/test $file_size");
-	system("chown zhengda.zhengda $dir/test");
 }
 
+system("tools/create_files data_files_RAID0.txt 640G RAID0 16");
+
+for (my $i = 1; $i <= 16; $i++) {
+	my $dev_idx = $i - 1;
+	my $dev_file = "/dev/sd$dev_names[$dev_idx]1";
+	my $dir = "/mnt/ssd${i}";
+	system("chown -R zhengda.zhengda $dir");
+	system("echo noop > /sys/block/sd$dev_names[$dev_idx]/queue/scheduler");
+	system("cat /sys/block/sd$dev_names[$dev_idx]/queue/scheduler");
+}
