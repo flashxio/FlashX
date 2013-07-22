4b8717f80b14e77650bdf3b80ed7862c07a06515
Da Zheng <zhengda1936@gmail.com>
2013-07-21 21:13:01 -0400

    print info in SSDIO.

--- IOR/aiori-SSDIO.c
+++ IOR/aiori-SSDIO.c
@@ -3,6 +3,7 @@
 
 void *IOR_Create_SSDIO(char *name, IOR_param_t *test)
 {
+	printf("SSDIO: create and open %s\n", name);
     IOR_offset_t fileSize = test->blockSize * test->segmentCount;
     if (test->filePerProc == FALSE) {
         fileSize *= test->numTasks;
@@ -15,6 +16,7 @@ void *IOR_Create_SSDIO(char *name, IOR_param_t *test)
 
 void *IOR_Open_SSDIO(char *name, IOR_param_t *test)
 {
+	printf("SSDIO: open %s\n", name);
 	int *fdp = (int *) malloc(sizeof(*fdp));
 	*fdp = ssd_open(name);
 	return (void *) fdp;
@@ -34,6 +36,7 @@ IOR_offset_t IOR_Xfer_SSDIO(int access, void *file, IOR_size_t *buffer,
 
 void IOR_Close_SSDIO(void *file, IOR_param_t *test)
 {
+	printf("SSDIO: close file\n");
 	int fd = *(int *) file;
 	ssd_close(fd);
 	free(file);
@@ -41,6 +44,7 @@ void IOR_Close_SSDIO(void *file, IOR_param_t *test)
 
 void IOR_Delete_SSDIO(char *file, IOR_param_t *test)
 {
+	printf("SSDIO: delete %s\n", file);
 	ssd_delete(file);
 }
 
@@ -54,5 +58,7 @@ void IOR_Fsync_SSDIO(void *file, IOR_param_t *test)
 
 IOR_offset_t IOR_GetFileSize_SSDIO(IOR_param_t *test, MPI_Comm comm, char *testFileName)
 {
-	return ssd_get_filesize(testFileName);
+	IOR_offset_t size = ssd_get_filesize(testFileName);
+	printf("file %s has %lld bytes\n", testFileName, size);
+	return size;
 }
