0f22a64b89bce15dc5187b5b0f5ebdf60472fd1b
Da Zheng <zhengda1936@gmail.com>
2013-09-09 00:02:13 -0400

    set_ssds.pl

--- scripts/set_ssds.pl
+++ scripts/set_ssds.pl
@@ -5,40 +5,55 @@ use strict;
 # this file mounts SSDs to their corresponding directories and sets
 # up data files correctly.
 
-# We assign a PCI bus to a NUMA node.
-# I hope there are few PCI buses than NUMA nodes.
-my $node_id = -1;
-my $curr_host = -1;
-
 my @dev_details = `ls -l /sys/block/ | grep sd | grep -v sda`;
-my $dev_idx = 1;
 
-open(my $fh, ">", "conf/data_files.txt");
+my %devices;
+my %host_ids;
+
 while (@dev_details) {
 	my $item = shift(@dev_details);
 	if ($item =~ /host([0-9]+).*sd([b-z])$/) {
 		my $host = $1;
 		my $dev = $2;
 
-		if ($curr_host != $host) {
-			$curr_host = $host;
-			$node_id++;
-		}
-		my $data_file = "$node_id:/mnt/ssd${dev_idx}/test_RAID0_16";
-		system("echo $data_file > conf/data_file${dev_idx}.txt");
-		print $fh "$data_file\n";
-
-		my $dev_file = "/dev/sd${dev}1";
-		my $mount_dir = "/mnt/ssd${dev_idx}";
-		print "dev: $dev_file, dir: $mount_dir\n" ;
-		system("mkdir -p $mount_dir");
-		system("mount $dev_file $mount_dir");
-		system("chown -R zhengda.zhengda $mount_dir");
-		system("echo noop > /sys/block/sd${dev}/queue/scheduler");
-		system("cat /sys/block/sd${dev}/queue/scheduler");
-		$dev_idx++;
+		# get all devices
+		$devices{$dev} = $host;
+		# get all host Ids.
+		$host_ids{$host} = 1;
 	}
 }
+
+# We assign a PCI bus to a NUMA node.
+# I hope there are few PCI buses than NUMA nodes.
+my $node_id = 0;
+for (sort {$a <=> $b} keys %host_ids) {
+	$host_ids{$_} = $node_id;
+	print "host $_ is assigned to node $node_id\n";
+	$node_id++;
+}
+
+my $dev_idx = 1;
+open(my $fh, ">", "conf/data_files.txt");
+for (sort keys %devices) {
+	my $dev = $_;
+	my $host = $devices{$dev};
+	my $node_id = $host_ids{$host};
+
+	my $data_file = "$node_id:/mnt/ssd${dev_idx}/test_RAID0_16";
+	system("echo $data_file > conf/data_file${dev_idx}.txt");
+	print $fh "$data_file\n";
+
+	my $dev_file = "/dev/sd${dev}1";
+	my $mount_dir = "/mnt/ssd${dev_idx}";
+	print "dev: $dev_file, dir: $mount_dir, on node $node_id\n" ;
+	system("mkdir -p $mount_dir");
+	system("mount $dev_file $mount_dir");
+	system("chown -R zhengda.zhengda $mount_dir");
+	system("echo noop > /sys/block/sd${dev}/queue/scheduler");
+	system("cat /sys/block/sd${dev}/queue/scheduler");
+	$dev_idx++;
+}
+
 close $fh;
 
 $dev_idx--;
