b83aa37447743cf34fb7362d87254b86b39901a1
Da Zheng <zhengda1936@gmail.com>
2013-09-27 11:11:26 -0400

    Add an assert in global_cached_io.

--- include/global_cached_private.h
+++ include/global_cached_private.h
@@ -149,6 +149,7 @@ public:
 	virtual int wait4complete(int num);
 	virtual void notify_completion(io_request *reqs[], int num);
 	virtual int num_pending_ios() const {
+		assert(num_issued_areqs.get() >= num_completed_areqs.get());
 		return num_issued_areqs.get() - num_completed_areqs.get();
 	}
 
