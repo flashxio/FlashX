b6b7bf8df5e2ca99c96ea704e6570e5a1aa7cecd
Da Zheng <zhengda1936@gmail.com>
2013-11-03 20:10:49 -0500

    [Test]: fix paths in the test set.

--- test/test_set.sh
+++ test/test_set.sh
@@ -1,66 +1,64 @@
 #!/bin/sh
 
 echo "this is the basic test for remote IO."
-./rand-read test/run_remote.txt test
+./test/test_rand_io test/conf/run_remote.txt test
 
 echo "this is the basic test for remote IO with verification enabled."
-./rand-read test/run_remote_virt.txt test
+./test/test_rand_io test/conf/run_remote_virt.txt test
 
 echo "this is the basic test for remote IO for writes to virtual SSDs."
-./rand-read test/run_remote_virt.txt test read_percent=0
+./test/test_rand_io test/conf/run_remote_virt.txt test read_percent=0
 
 echo "this is the basic test for remote IO. Large read."
 echo "check the average request size. It should be 128 sectors."
-./rand-read test/run_remote.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
+./test/test_rand_io test/conf/run_remote.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
 
 echo "this is the basic test for remote IO read from virtual SSDs with verification enabled. Large read."
-./rand-read test/run_remote_virt.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
+./test/test_rand_io test/conf/run_remote_virt.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
 
 echo "this is the basic test for remote IO write to virtual SSDs with verification enabled. Large write."
-./rand-read test/run_remote_virt.txt test entry_size=$((4096 * 32)) read_percent=0 RAID_block_size=64K
+./test/test_rand_io test/conf/run_remote_virt.txt test entry_size=$((4096 * 32)) read_percent=0 RAID_block_size=64K
 
 echo "this is the basic test for remote IO. Large read."
 echo "check the average request size. It should be 256 sectors. This is to test the code path of splitting a request."
-./rand-read test/run_remote.txt test entry_size=$((4096 * 32))
+./test/test_rand_io test/conf/run_remote.txt test entry_size=$((4096 * 32))
 
 
 echo "the basic test for global cached read."
-./rand-read test/run_cache.txt test
+./test/test_rand_io test/conf/run_cache.txt test
 
 echo "the basic test for global cached read on virtual SSDs"
-./rand-read test/run_cache_virt.txt test
+./test/test_rand_io test/conf/run_cache_virt.txt test
 
 echo "the basic test for global cached write on virtual SSDs"
-./rand-read test/run_cache_virt.txt test read_percent=0
+./test/test_rand_io test/conf/run_cache_virt.txt test read_percent=0
 
 echo "the basic test for global cached IO with small reads on virtual SSDs"
-./rand-read test/run_cache_virt.txt test pages=$((1024 * 1024)) entry_size=$((128))
+./test/test_rand_io test/conf/run_cache_virt.txt test pages=$((1024 * 1024)) entry_size=$((128))
 
 echo "the basic test for global cached IO with large reads"
-./rand-read test/run_cache.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
+./test/test_rand_io test/conf/run_cache.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
 
 echo "the basic test for global cached IO with large reads on virtual SSDs"
-./rand-read test/run_cache_virt.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
+./test/test_rand_io test/conf/run_cache_virt.txt test entry_size=$((4096 * 32)) RAID_block_size=64K
 
 echo "the basic test for global cached IO with small writes on virtual SSDs. Large writes and cache flusher are enabled by default."
-./rand-read test/run_cache_virt.txt test pages=$((1024 * 1024)) entry_size=$((128)) read_percent=0
+./test/test_rand_io test/conf/run_cache_virt.txt test pages=$((1024 * 1024)) entry_size=$((128)) read_percent=0
 
 echo "the basic test for global cached IO with large writes on virtual SSDs."
-./rand-read test/run_cache_virt.txt test read_percent=0 entry_size=$((4096 * 32)) RAID_block_size=64K
+./test/test_rand_io test/conf/run_cache_virt.txt test read_percent=0 entry_size=$((4096 * 32)) RAID_block_size=64K
 
 echo "the basic test for global cached IO under the TPCC workload."
-./rand-read test/run_cache_tpcc.txt test
+./test/test_rand_io test/conf/run_cache_tpcc.txt test
 
 echo "the basic test for global cached IO under the Neo4j workload."
-./rand-read test/run_cache_tpcc.txt test workload=test/workload/DijkstraSearch-reconstructed.data
+./test/test_rand_io test/conf/run_cache_tpcc.txt test workload=test/workload/DijkstraSearch-reconstructed.data
 
 echo "the basic test for parted global cached read on virtual SSDs."
-./rand-read test/run_parted_cache_virt.txt test
+./test/test_rand_io test/conf/run_parted_cache_virt.txt test
 
 echo "the basic test for parted global cached IO under the TPCC workload on virtual SSDs."
-./rand-read test/run_parted_cache_tpcc.txt test
+./test/test_rand_io test/conf/run_parted_cache_tpcc.txt test
 
-
-# failed test
-# The problem is that it fails to activate a thread.
-./rand-read test/run_parted_cache_tpcc.txt test io_depth=32
+echo "the basic test for parted global cached IO under the TPCC workload with few parallel IOs on virtual SSDs."
+./test/test_rand_io test/conf/run_parted_cache_tpcc.txt test io_depth=32
