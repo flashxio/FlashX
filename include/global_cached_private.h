db5078cf9d0ad03320bc8df757d18a485b3d6e07
Da Zheng <zhengda1936@gmail.com>
2013-11-06 16:41:32 -0500

    [SAFS]: fix a compilation error when STATISTICS is disabled.

--- include/global_cached_private.h
+++ include/global_cached_private.h
@@ -206,8 +206,10 @@ public:
 #endif
 
 	virtual void print_state() {
+#ifdef STATISTICS
 		printf("global cached io %d has %d pending reqs and %d reqs from underlying\n",
 				get_io_idx(), num_pending_ios(), num_from_underlying.get());
+#endif
 		printf("%d completed pending reqs, %d queued completed reqs from underlying\n",
 				pending_requests.get_num_entries(), complete_queue.get_num_entries());
 		underlying->print_state();
