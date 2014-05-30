523f1e731b9091aee87c54b292b79cdcad64aaae
Disa Mhembere <dmhembe1@jhu.edu>
2014-05-30 13:19:30 -0400

    [Bug]: Fixing Transitivity. Not compiling yet

--- flash-graph/transitivity/graph_transitivity.cpp
+++ flash-graph/transitivity/graph_transitivity.cpp
@@ -28,7 +28,65 @@
   #include "omp.h"
 #endif
 
+
+class trans_vertex : public compute_vertex
+{
+  public:
+    trans_vertex() {
+    }
+
+    trans_vertex(vertex_id_t& id, const vertex_index& index) : 
+      compute_vertex(vertex_id_t& id, const vertex_index& index) {
+    }
+    void run(vertex_program &);
+    void run(vertex_program &, const page_vertex &);
+    void run_on_message(vertex_program &, const vertex_message);
+};
+
 // TODO: Add includes for triangles
+
+/**
+* Transitivity = 2#triangles / (deg * (deg - 1))
+**/
+FG_vector<vsize_t>::ptr compute_transitivity(FG_graph::ptr fg)
+{
+  graph_index::ptr index = NUMA_graph_index<trans_vertex>::create(
+      fg->get_index_file());
+  graph_engine::ptr graph = graph_engine::create(fg->get_graph_file(),
+      index, fg->get_configs());
+
+  if (!graph_conf.get_prof_file().empty())
+    ProfilerStart(graph_conf.get_prof_file().c_str());
+
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  printf("transitivity starts\n");
+  // TODO: Get triangles
+  FG_vector<vsize_t>::ptr tri_v; //= fg->compute_triangles(); // Vector with triangles
+ 
+  // Compute degree 
+  FG_vector<vsize_t>::ptr degree_v; // really (deg * (deg - 1))
+  
+(void) omp_set_num_threads(graph->get_num_threads()); // TODO: Will this work without starting? Prolly not
+#pragma omp parallel for 
+    for (vsize_t vid = graph->get_min_vertex_id(); 
+        vid <= graph->get_max_vertex_id(); vid++) {
+
+      vsize_t tmp =  graph->get_vertex_edges(vid);
+      degree_v->set(vid, tmp*(tmp-1)); 
+    }
+#pragma omp parallel for private(degree_v, tri_v) // Mutate degree_v to save space
+    for (vsize_t i = 0; i < degree_v->get_size(); i++) {
+      tri_v->get(i) > 0 ? degree_v->set(i, (2*tri_v->get(i)/degree_v->get(i))) 
+                      : degree_v->set(i,0); // Handles case degree = 0
+    }
+
+  gettimeofday(&end, NULL);
+  printf("Transitivity takes %f seconds\n", time_diff(start, end));
+  return degree_v;
+}
+
 
 void int_handler(int sig_num)
 {
@@ -53,7 +111,6 @@ int main(int argc, char *argv[])
 	int opt;
 	std::string confs;
 	std::string output_file;
-	size_t min_comp_size = 0;
 	int num_opts = 0;
 	bool preload = false;
 	while ((opt = getopt(argc, argv, "c:o:p:")) != -1) {
@@ -91,44 +148,12 @@ int main(int argc, char *argv[])
 
 	signal(SIGINT, int_handler);
 
-	FG_graph::ptr graph = FG_graph::create(graph_file, index_file, configs);
-  FG_vector<vsize_t> vec = compute_transitivity(graph);
+  graph_index::ptr index = NUMA_graph_index<trans_vertex>::create(index_file);
+  graph_engine::ptr graph = graph_engine::create(graph_file, index, configs);
+
+  if (preload)
+    graph->preload_graph();
+  // FG_vector<vsize_t> vec = compute_transitivity(graph);
   // TODO: Print me
 
 }
-/**
-* Transitivity = 2#triangles / (deg * (deg - 1))
-**/
-FG_vector<vsize_t>::ptr compute_transitivity(FG_graph::ptr fg)
-{
-  graph_index::ptr index = NUMA_graph_index<wcc_vertex>::create(
-      fg->get_index_file());
-  graph_engine::ptr graph = graph_engine::create(fg->get_graph_file(),
-      index, fg->get_configs());
-
-  struct timeval start, end;
-  printf("transitivity starts\n");
-  // TODO: Get triangles
-  FG_vector<vsize_t> tri_v; // = fg->compute_triangles(); // Vector with triangles
- 
-  // Compute degree 
-  FG_vector<vsize_t> degree_v; // really (deg * (deg - 1))
-  vsize_t vid = graph->get_min_vertex_id();
-
-(void) omp_set_num_threads(graph.get_num_threads()); // TODO: Will this work without starting? Prolly not
-
-#pragma omp parallel for private(vid) 
-    for (; vid < graph->get_max_vertex_id(); vid++) {
-      vsize_t tmp =  graph->get_vertex_edges(vid);
-      degree_v[vid] = tmp*(tmp-1); // May be zero 
-    }
-#pragma omp parallel for private(degree_v, tri_v) // Mutate degree_v to save space
-    for (vsize_t i = 0; i < degree_v.size(); i++) {
-      tri_v[i] > 0 ? degree_v[i] = 2*tri_v[i]/degree_v[i] : degree_v[i] = 0; 
-    }
-
-  printf("Transitivity takes %f seconds\n", time_diff(start, end));
-  return degree_v;
-}
-
-
