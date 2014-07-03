b83703383ea65872cea5dccabc9485699622bae6
Disa Mhembere <dmhembe1@jhu.edu>
2014-07-03 17:50:10 -0400

    [Graph]: Matrix based k-means works with artifical eigs but not real

--- flash-graph/clustering/matrix_k_means.cpp
+++ flash-graph/clustering/matrix_k_means.cpp
@@ -17,24 +17,16 @@
  * limitations under the License.
  */
 
-// #include <signal.h>
-#include <google/profiler.h>
 #include <stdlib.h>
-
+#include <math.h>
+#ifdef PROFILER
+#include <google/profiler.h>
+#endif
 #include <vector>
 #include <limits> 
 
-#include "thread.h"
-#include "io_interface.h"
-#include "container.h"
-#include "concurrency.h"
-
-#include "vertex_index.h"
 #include "FGlib.h"
-#include "graph_engine.h"
-#include "graph.h"
-#include "graph_config.h"
-#include "math.h"
+#include "matrix/FG_sparse_matrix.h"
 #include "matrix/FG_dense_matrix.h"
 #include "matrix/matrix_eigensolver.h"
 
@@ -45,33 +37,41 @@ vsize_t g_NV;
 float g_MIN_EIGV = std::numeric_limits<float>::max();
 float g_MAX_EIGV = std::numeric_limits<float>::min();
 
-enum phase_t {
-  e_step, /* Update cluster_id for each vertex */
-  m_step, /* Update cluster_means for each cluster */
-};
-
-// TODO: Fill in
-ev_float_t L2Norm(FG_vector<ev_float_t>::ptr, FG_vector<ev_float_t>::ptr) {
-  return 0.0;
+/**
+  * \brief Get Euclidean distance given two vectors.
+  */
+ev_float_t L2Norm(FG_vector<ev_float_t>::ptr v1, FG_vector<ev_float_t>::ptr v2) {
+  assert(v1->get_size() == v2->get_size());
+  ev_float_t sum = 0;
+  for (size_t idx = 0; idx < v1->get_size(); idx++) {
+    ev_float_t diff = v1->get(idx) - v2->get(idx);
+    sum += diff*diff;
+  }
+  if (sum == 0) { return 0; }   
+  return sqrt(sum);
 }
 
 float gen_random_float() {
-    float random = ((float) rand()) / (float) RAND_MAX;
-    float diff = g_MAX_EIGV - g_MIN_EIGV;
-    float r = random * diff;
-    return g_MIN_EIGV + r;
+  float random = ((float) rand()) / (float) RAND_MAX;
+  float diff = g_MAX_EIGV - g_MIN_EIGV;
+  float r = random * diff;
+  return g_MIN_EIGV + r;
 }
 
-void print_func(vertex_id_t i) {
-  std::cout << " " << i;
-}
-void print_vector(std::vector<float> v) {
+// Begin Helpers
+template <typename T>
+void print_vector(typename std::vector<T> v) {
   std::cout << "[";
-    for_each (v.begin(), v.end(), print_func);
+
+  typename std::vector<T>::iterator itr = v.begin();
+    for (; itr != v.end(); itr++) {
+      std::cout << " "<< *itr;
+    }
   std::cout <<  " ]\n";
 }
 
-void print_FG_vector(FG_vector<ev_float_t>::ptr fgv) {
+template <typename T>
+void print_FG_vector(typename FG_vector<T>::ptr fgv) {
   std::cout << "[";
     for (vsize_t i=0; i < fgv->get_size(); i++) {
       std::cout << " " << fgv->get(i);
@@ -80,14 +80,6 @@ void print_FG_vector(FG_vector<ev_float_t>::ptr fgv) {
   
 }
 // End Helpers
-
-
-void int_handler(int sig_num)
-{
-	if (!graph_conf.get_prof_file().empty())
-		ProfilerStop();
-	exit(0);
-}
 
 void print_usage()
 {
@@ -165,16 +157,16 @@ int main(int argc, char *argv[])
 
   FG_graph::ptr _graph = FG_graph::create(graph_file, index_file, configs);
 
-#if 0
+#if 1
   if (matrix_type == "adj") {
     FG_adj_matrix::ptr matrix = FG_adj_matrix::create(_graph);
-    // compute_eigen<FG_adj_matrix>(matrix, m, g_NV, which, g_eigen_pairs);
-  }
+    compute_eigen<FG_adj_matrix>(matrix, m, g_NV, which, g_eigen_pairs);
+    }
   else
     assert (0);
 #endif
 
-# if 1
+# if 0
   // Fake eigen computation for WIKI
   std::cout << "Creating Fake eigs ..." << std::endl;
   for (vsize_t i = 0; i < g_NV; i++) {
@@ -189,38 +181,54 @@ int main(int argc, char *argv[])
     g_eigen_pairs.push_back(ep); 
   }
 # endif
-  
-  // Make vector for cluster assignments
-  FG_vector<vsize_t>::ptr cluster_assignments = 
-    FG_vector<vsize_t>::create(g_eigen_pairs.size());
-  cluster_assignments->init(INVALID_VERTEX_ID);
 
   // Create row-wise dense matrix to hold eigs
-  // FG_col_wise_matrix<ev_float_t>::ptr eigs = 
-  //  FG_col_wise_matrix<ev_float_t>::create(g_eigen_pairs.size(), g_NV); // (n, x)
   FG_row_wise_matrix<ev_float_t>::ptr eigs = 
-    FG_row_wise_matrix<ev_float_t>::create(g_eigen_pairs.size(), g_NV); // (n, x)
+    FG_row_wise_matrix<ev_float_t>::create(g_eigen_pairs[0].second->get_size(), g_NV); // (n, x)
+  eigs->resize(g_eigen_pairs[0].second->get_size(), g_NV);
+
+  // Make vector for cluster assignments
+  FG_vector<vsize_t>::ptr cluster_assignments = 
+    FG_vector<vsize_t>::create(eigs->get_num_rows());
+  cluster_assignments->init(INVALID_VERTEX_ID);
+
+  FG_vector<vsize_t>::ptr prev_cluster_assignments = 
+    FG_vector<vsize_t>::create(eigs->get_num_rows());
+  cluster_assignments->init(INVALID_VERTEX_ID);
+
+#ifdef PROFILER
+  ProfilerStart("/home/disa/graph-engine/flash-graph/clustering/profile_out.log");
+#endif
+
 
   // Insert eigs into `eig matrix` and discard eigenvalues
-  // FIXME: Slow I assume ...
+  // FIXME: Slow .. I assume ...
   vsize_t col = 0;
+
   BOOST_FOREACH(eigen_pair_t &v, g_eigen_pairs) {
     for (vsize_t row = 0; row < v.second->get_size(); row++) {
       eigs->set(row, col, v.second->get(row));
+      
+      // For random init we need a baseline
+      ev_float_t tmp = v.second->min();
+      if (tmp < g_MIN_EIGV) { g_MIN_EIGV = tmp; }
+      tmp = v.second->max();
+      if (tmp > g_MAX_EIGV) { g_MAX_EIGV = tmp; }
     }
     col++;
   }
-  
+
   // Create `clusters` dense matrix
   FG_row_wise_matrix<ev_float_t>::ptr clusters = 
     FG_row_wise_matrix<ev_float_t>::create(K, g_NV); // (K, x)
+  clusters->resize(K, g_NV);
 
   // Figure out max and min so we can use it to evenly distribute cluster init
   printf("\nNew g_MAX_EIGV is %f ...\n", g_MAX_EIGV);
-  printf("\nNew g_MIN_EIGV is %f ...\n", g_MIN_EIGV);
+  printf("New g_MIN_EIGV is %f ...\n", g_MIN_EIGV);
 
   // Randomly initialize cluster centers
-  for (vsize_t row = 0; row < K; row++) {
+  for (vsize_t clusterID = 0; clusterID < K; clusterID++) {
     FG_vector<ev_float_t>::ptr fgv = FG_vector<ev_float_t>::create(g_NV);
 
     #pragma omp parallel for 
@@ -228,56 +236,91 @@ int main(int argc, char *argv[])
       fgv->set(j, gen_random_float());
     }
 
-    clusters->set_row(row, *fgv);
+    clusters->set_row(clusterID, fgv);
   }
-  
+ 
   // Create matrix to hold distances
   FG_row_wise_matrix<ev_float_t>::ptr distance_matrix = 
     FG_row_wise_matrix<ev_float_t>::create(eigs->get_num_rows(), K); // (n, K) 
+  distance_matrix->resize(eigs->get_num_rows(), K);
   
 	printf("Matrix K-means starting ... \n");
-	printf("prof_file: %s\n", graph_conf.get_prof_file().c_str());
-	if (!graph_conf.get_prof_file().empty())
-		ProfilerStart(graph_conf.get_prof_file().c_str());
 
   struct timeval start, end;
   gettimeofday(&start, NULL);
 
   std::cout << "Computing " << g_MAX_ITERS << " iterations\n";
   for (vsize_t iter = 0; iter < g_MAX_ITERS; iter++) {
-    printf("Iteration %d ... Computing distance matrix ...\n", iter);
-    // Compute distances. TODO: Make coalesced OMP loop
+    // Hold cluster assignment counter
+    std::vector<vsize_t> cluster_assignment_counts;
+    cluster_assignment_counts.assign(K, 0);
 
-    for (vsize_t vertexID = 0;  vertexID < eigs->get_num_rows(); vertexID++) {
+    printf("E-step Iteration %d ... Computing distance matrix ...\n", iter);
+    // Compute distances. 
+    // TODO: Make coalesced OMP loop
+
+    for (vsize_t vertexID = 0; vertexID < eigs->get_num_rows(); vertexID++) {
       for (vsize_t clusterID = 0; clusterID < clusters->get_num_rows(); clusterID++) {
+        
+        // printf("Cluster ID: %u has mean ", clusterID);
+        // print_FG_vector<ev_float_t>(clusters->get_row_ref(clusterID));
+
         distance_matrix->set(vertexID, clusterID, 
             L2Norm(eigs->get_row_ref(vertexID), clusters->get_row_ref(clusterID)));
       }
-    cluster_assignments->set(vertexID, distance_matrix->get_row_ref(vertexID)->argmin());
+
+      // printf("Vertex %u has distance vector: ", vertexID); 
+      // print_FG_vector<ev_float_t>(distance_matrix->get_row_ref(vertexID));
+
+      size_t assigned_cluster = distance_matrix->get_row_ref(vertexID)->argmin();
+
+      // printf("Vertex %u with eig: %f, is assigned cluster %lu\n", vertexID, eigs->get(vertexID, 0), assigned_cluster);
+
+    cluster_assignments->set(vertexID, assigned_cluster);
+    cluster_assignment_counts[assigned_cluster]++;
     }
+
+    // printf("Previous clusters: "); print_FG_vector<vsize_t>(prev_cluster_assignments);
+    // printf("\nNew clusters: "); print_FG_vector<vsize_t>(cluster_assignments); 
+    
+    if (cluster_assignments->eq_all(prev_cluster_assignments)) {
+      printf("K-means converged in %u iterations\n", iter);
+      break;
+    }
+
+    printf("Cluster assignment counts: \n");
+    print_vector(cluster_assignment_counts);
   
+    // I clear the cluster mean matrix because I don't want to use more memory
     printf("Clearing cluster means ...\n");
-    for (vsize_t clusterID = 0; clusterID < clusters->get_num_rows(); clusterID++) {
-      clusters->get_row_ref(clusterID)->assign(0); // TODO: Make row_wise_mat support this in ||
-    }
+    clusters->assign_all(0.0);
 
-    printf("Updating cluster means ...\n");
+    printf("M-step Updating cluster means ...\n");
     for (vsize_t vertexID = 0; vertexID < cluster_assignments->get_size(); vertexID++) {
-       clusters->get_row_ref(cluster_assignments->get(vertexID)) += eigs->get_row_ref(vertexID);
+      printf("Vertex %u ...\n", vertexID);
+      (clusters->get_row_ref(cluster_assignments->get(vertexID)))->plus_eq( eigs->get_row_ref(vertexID) );
     }
-    // TODO: Take the mean of all added means
-
+    // Take the mean of all added means
+      printf("Div by in place ...\n");
+    for (vsize_t clusterID = 0; clusterID < clusters->get_num_rows(); clusterID++) {
+      clusters->get_row_ref(clusterID)->div_by_in_place
+        (cluster_assignment_counts[clusterID]); 
+    }
+    
+    // Set prev to current
+    prev_cluster_assignments->shallow_copy(cluster_assignments);  
   }
+
+#ifdef PROFILER
+  ProfilerStop();
+#endif
   gettimeofday(&end, NULL);
 
   printf("\n******************************************\n"
-      "K-means converges in %f seconds\n"
+      "K-means complete in %f seconds\n"
       "\n******************************************\n",
       time_diff(start, end));
 
   // printf("Printing clusters: \n");
   // print_FG_vector(cluster_assignments);
-  
-	if (!graph_conf.get_prof_file().empty())
-		ProfilerStop();
 }
