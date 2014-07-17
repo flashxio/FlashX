502ef03d4de6adb329f32cdb54af2fec71af0e3b
Da Zheng <zhengda1936@gmail.com>
2014-07-17 16:31:49 -0400

    [Graph]: make graph-stat work on time-series graphs.

--- flash-graph/tools/graph-stat.cpp
+++ flash-graph/tools/graph-stat.cpp
@@ -27,24 +27,77 @@
 #include "graph_config.h"
 #include "stat.h"
 #include "FGlib.h"
+#include "ts_graph.h"
 
 const int POWER_CONST = 10;
 
+void print_usage()
+{
+	fprintf(stderr, "stat [options] conf_file graph_file index_file\n");
+	graph_conf.print_help();
+	params.print_help();
+}
+
 int main(int argc, char *argv[])
 {
-	if (argc < 4) {
-		fprintf(stderr, "stat conf_file graph_file index_file\n");
-		graph_conf.print_help();
-		params.print_help();
+	int opt;
+	int num_opts = 0;
+
+	std::string start_time_str;
+	std::string time_unit_str;
+	time_t time_interval = 1;
+	time_t start_time = 0;
+	while ((opt = getopt(argc, argv, "t:l:u:")) != -1) {
+		num_opts++;
+		switch (opt) {
+			case 't':
+				start_time_str = optarg;
+				num_opts++;
+				break;
+			case 'l':
+				time_interval = atol(optarg);
+				num_opts++;
+				break;
+			case 'u':
+				time_unit_str = optarg;
+				num_opts++;
+				break;
+			default:
+				print_usage();
+		}
+	}
+	argv += 1 + num_opts;
+	argc -= 1 + num_opts;
+
+	if (argc < 3) {
+		print_usage();
 		exit(-1);
 	}
 
-	std::string conf_file = argv[1];
-	std::string graph_file = argv[2];
-	std::string index_file = argv[3];
+	std::string conf_file = argv[0];
+	std::string graph_file = argv[1];
+	std::string index_file = argv[2];
 
 	config_map configs(conf_file);
 	graph_conf.init(configs);
+
+	bool on_ts = false;
+	if (!start_time_str.empty()) {
+		on_ts = true;
+		if (!time_unit_str.empty()) {
+			if (time_unit_str == "hour")
+				time_interval *= HOUR_SECS;
+			else if (time_unit_str == "day")
+				time_interval *= DAY_SECS;
+			else if (time_unit_str == "month")
+				time_interval *= MONTH_SECS;
+			else
+				fprintf(stderr, "a wrong time unit: %s\n", time_unit_str.c_str());
+		}
+
+		start_time = conv_str_to_time(start_time_str);
+		printf("start time: %ld, interval: %ld\n", start_time, time_interval);
+	}
 
 	FG_graph::ptr fg = FG_graph::create(graph_file, index_file, conf_file);
 	graph_header header = get_graph_header(fg);
@@ -57,12 +110,6 @@ int main(int argc, char *argv[])
 			break;
 		case graph_type::UNDIRECTED:
 			graph_type_str = "undirected";
-			break;
-		case graph_type::TS_DIRECTED:
-			graph_type_str = "time-series directed";
-			break;
-		case graph_type::TS_UNDIRECTED:
-			graph_type_str = "time-series undirected";
 			break;
 		default:
 			assert(0);
@@ -79,8 +126,16 @@ int main(int argc, char *argv[])
 
 	bool directed = header.is_directed_graph();
 	if (directed) {
-		FG_vector<vsize_t>::ptr in_degrees = get_degree(fg, IN_EDGE);
-		FG_vector<vsize_t>::ptr out_degrees = get_degree(fg, OUT_EDGE);
+		FG_vector<vsize_t>::ptr in_degrees;
+		FG_vector<vsize_t>::ptr out_degrees;
+		if (on_ts) {
+			in_degrees = get_ts_degree(fg, IN_EDGE, start_time, time_interval);
+			out_degrees = get_ts_degree(fg, OUT_EDGE, start_time, time_interval);
+		}
+		else {
+			in_degrees = get_degree(fg, IN_EDGE);
+			out_degrees = get_degree(fg, OUT_EDGE);
+		}
 		size_t tot_in_edges = in_degrees->sum<size_t>();
 		size_t tot_out_edges = out_degrees->sum<size_t>();
 		assert(tot_in_edges == tot_out_edges);
@@ -107,7 +162,11 @@ int main(int argc, char *argv[])
 		hist_out_edges.print(stdout);
 	}
 	else {
-		FG_vector<vsize_t>::ptr degrees = get_degree(fg, BOTH_EDGES);
+		FG_vector<vsize_t>::ptr degrees;
+		if (on_ts)
+			degrees = get_ts_degree(fg, BOTH_EDGES, start_time, time_interval);
+		else
+			degrees = get_degree(fg, BOTH_EDGES);
 		size_t tot_edges = degrees->sum<size_t>();
 		vsize_t max_num_edges = degrees->max();
 		printf("There are %ld edges\n", tot_edges);
