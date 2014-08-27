0a794af8542aee6201f02d5da072dc6f01527f5b
Da Zheng <zhengda1936@gmail.com>
2014-08-27 14:18:44 -0400

    [SAFS]: setup SSDs with partitions in them.

--- conf/set_ssds.pl
+++ conf/set_ssds.pl
@@ -28,11 +28,23 @@ print "There are $num_devs SSDs\n";
 my %devices;
 my %host_ids;
 
+sub get_main_devname {
+	my $dev = $_;
+	if ($dev =~ /(sd[b-z])[0-9]+/) {
+		return $1;
+	}
+	else {
+		return $dev;
+	}
+}
+
 foreach (@dev_names) {
-	my $item = `ls -l /sys/block/ | grep $_`;
+	my $dev_name = $_;
+	my $main_name = get_main_devname($_);
+	my $item = `ls -l /sys/block/ | grep $main_name`;
 	if ($item =~ /host([0-9]+).*sd([b-z])$/) {
 		my $host = $1;
-		my $dev = $2;
+		my $dev = $dev_name;
 
 		# get all devices
 		$devices{$dev} = $host;
@@ -54,21 +66,22 @@ my $dev_idx = 1;
 open(my $fh, ">", "conf/data_files.txt");
 for (sort keys %devices) {
 	my $dev = $_;
+	my $main_name = get_main_devname($dev);
 	my $host = $devices{$dev};
 	my $node_id = $host_ids{$host};
 
 	my $data_file = "$node_id:/mnt/ssd${dev_idx}/";
 	print $fh "$data_file\n";
 
-	my $dev_file = "/dev/sd${dev}";
+	my $dev_file = "/dev/${dev}";
 	my $mount_dir = "/mnt/ssd${dev_idx}";
 	print "dev: $dev_file, dir: $mount_dir, on node $node_id\n" ;
 	system("mkdir -p $mount_dir");
 	system("mount $dev_file $mount_dir");
 	system("chown -R zhengda.zhengda $mount_dir");
-	system("echo noop > /sys/block/sd${dev}/queue/scheduler");
-	system("cat /sys/block/sd${dev}/queue/scheduler");
-	system("echo 2 > /sys/block/sd${dev}/queue/rq_affinity");
+	system("echo noop > /sys/block/${main_name}/queue/scheduler");
+	system("cat /sys/block/${main_name}/queue/scheduler");
+	system("echo 2 > /sys/block/${main_name}/queue/rq_affinity");
 	$dev_idx++;
 }
 
