08d8c0bf48b4be35ee84a3036b21049976c750da
Da Zheng <zhengda1936@gmail.com>
2014-10-12 11:28:04 -0400

    [Graph]: declare PACKAGE in R wrapper functions.

--- flash-graph/Rpkg/R/flashgraph.R
+++ flash-graph/Rpkg/R/flashgraph.R
@@ -8,36 +8,37 @@ get_fg_graph <- function(graph.file, index.file, conf.file)
 fg.clusters <- function(graph, mode=c("weak", "strong"))
 {
 	if (mode == "weak")
-		.Call("R_FG_compute_wcc", graph)
+		.Call("R_FG_compute_wcc", graph, PACKAGE="FlashGraph")
 	else if (mode == "strong")
-		.Call("R_FG_compute_scc", graph)
+		.Call("R_FG_compute_scc", graph, PACKAGE="FlashGraph")
 	else
 		stop("a wrong mode")
 }
 
 fg.transitivity <- function(graph)
 {
-	.Call("R_FG_compute_transitivity", graph)
+	.Call("R_FG_compute_transitivity", graph, PACKAGE="FlashGraph")
 }
 
 fg.degree <- function(graph, type="both")
 {
-	.Call("R_FG_get_degree", graph, type)
+	.Call("R_FG_get_degree", graph, type, PACKAGE="FlashGraph")
 }
 
 fg.page.rank <- function(graph, no.iters=1000, damping.factor=0.85)
 {
-	.Call("R_FG_compute_pagerank", graph, no.iters, damping.factor)
+	.Call("R_FG_compute_pagerank", graph, no.iters, damping.factor,
+		  PACKAGE="FlashGraph")
 }
 
 fg.directed.triangles <- function(graph, type="cycle")
 {
-	.Call("R_FG_compute_directed_triangles", graph, type)
+	.Call("R_FG_compute_directed_triangles", graph, type, PACKAGE="FlashGraph")
 }
 
-fg.local.scan <- function(graph, k=1)
+fg.undirected.triangles <- function(graph)
 {
-	.Call("R_FG_compute_local_scan", graph, k)
+	.Call("R_FG_compute_undirected_triangles", graph, PACKAGE="FlashGraph")
 }
 
 .onLoad <- function(libname, pkgname) {
