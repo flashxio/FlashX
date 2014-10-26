c511c12ed74b412ecd3255bb9ac3ef3bc85b5001
Da Zheng <zhengda1936@gmail.com>
2014-10-26 18:38:01 -0400

    [SAFS]: fix CMakeLists.txt

---
+++ cmake/Findstxxl.cmake
@@ -0,0 +1,9 @@
+find_path(LIBSTXXL_INCLUDE_DIRS NAMES stxxl.h)
+find_library(LIBSTXXL_LIBRARIES NAMES stxxl)
+
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Libstxxl DEFAULT_MSG
+	LIBSTXXL_LIBRARIES
+	LIBSTXXL_INCLUDE_DIRS)
+
+mark_as_advanced(LIBSTXXL_INCLUDE_DIRS LIBSTXXL_LIBRARIES)
