b6f9f7685c0715e20c4d56220ea9e58d77eebcba
Da Zheng <zhengda1936@gmail.com>
2014-11-09 09:11:01 -0500

    [Graph]: release test verifies the generated graphs.

--- flash-graph/release-test/run_release_test.sh
+++ flash-graph/release-test/run_release_test.sh
@@ -6,7 +6,7 @@ wget http://snap.stanford.edu/data/wiki-Vote.txt.gz
 gunzip wiki-Vote.txt.gz
 rm wiki-Vote.adj*
 rm wiki-Vote.index*
-../tools/el2al -w wiki-Vote.adj wiki-Vote.index wiki-Vote.txt
+../tools/el2al -v -w wiki-Vote.adj wiki-Vote.index wiki-Vote.txt
 ../../utils/SAFS-util run_test.txt load wiki-Vote-v4 wiki-Vote.adj-v4
 ../../utils/SAFS-util run_test.txt load wiki-Vote-index-v4 wiki-Vote.index-v4
 sed '/^#/d' wiki-Vote.txt > wiki-Vote1.txt
@@ -16,7 +16,7 @@ wget http://snap.stanford.edu/data/facebook_combined.txt.gz
 gunzip facebook_combined.txt.gz
 rm facebook.adj*
 rm facebook.index*
-../tools/el2al -u -w facebook.adj facebook.index facebook_combined.txt
+../tools/el2al -v -u -w facebook.adj facebook.index facebook_combined.txt
 ../../utils/SAFS-util run_test.txt load facebook-v4 facebook.adj-v4
 ../../utils/SAFS-util run_test.txt load facebook-index-v4 facebook.index-v4
 sed '/^#/d' facebook_combined.txt > facebook_combined1.txt
