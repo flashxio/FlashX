99749e35c672efb7f7cc279150d704169fac6ce4
Da Zheng <zhengda1936@gmail.com>
2014-11-13 21:42:26 -0500

    [Graph]: generate plain text files for .Rd files properly.

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo 'roxygen2::roxygenize("flash-graph/Rpkg/")' | R --no-save
-mkdir -p doc/FlashGraphR
+mkdir -p docs/FlashGraphR
 for file in `ls flash-graph/Rpkg/man/`
 do
-	R CMD Rd2txt "flash-graph/Rpkg/man/$file" -o "doc/FlashGraphR/$file"
+	input_file="flash-graph/Rpkg/man/$file"
+	output_file="docs/FlashGraphR/${file}.txt"
+	echo "tools::Rd2txt(\"$input_file\", out=\"$output_file\", options=list(underline_titles=FALSE))" | R --no-save
 done
