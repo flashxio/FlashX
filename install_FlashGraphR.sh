7f9f3ad55ba549a6d55a45089d47280a38201fcd
Da Zheng <zhengda1936@gmail.com>
2014-11-15 20:09:24 -0500

    [Graph]: simplify install script for FlashGraphR.

--- install_FlashGraphR.sh
+++ install_FlashGraphR.sh
@@ -8,7 +8,8 @@ if [ -z "$path" ]; then
 	path=`find /usr/local -name Rcpp.h`
 fi
 
-matrix_path=`find build -name libmatrix.a`
+fg_lib=`pwd`/build
+matrix_path=`find $fg_lib/flash-graph -name libmatrix.a`
 if [ -n "$matrix_path" ]; then
 	echo "find libmatrix.a"
 	export FG_EIGEN=1
@@ -16,8 +17,8 @@ fi
 if [ -n "$path" ]; then
 	path=`dirname $path`
 	echo $path
-	RCPP_INCLUDE=$path FG_DIR=`pwd` FG_LIB=`pwd`/build R CMD INSTALL FlashGraphR_0.3-0.tar.gz
+	RCPP_INCLUDE=$path FG_DIR=`pwd` FG_LIB=$fg_lib R CMD INSTALL FlashGraphR_0.3-0.tar.gz
 else
-	FG_DIR=`pwd` FG_LIB=`pwd`/build R CMD INSTALL FlashGraphR_0.3-0.tar.gz
+	FG_DIR=`pwd` FG_LIB=$fg_lib R CMD INSTALL FlashGraphR_0.3-0.tar.gz
 fi
 rm FlashGraphR_0.3-0.tar.gz
