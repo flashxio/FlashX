58b9bdc51bed50b43fccc2113eed25e80d43eeb8
Da Zheng <zhengda1936@gmail.com>
2014-11-20 12:26:19 -0500

    [Matrix]: unit-test steal I/O.

--- matrix/unit-test/test_io_gen.cpp
+++ matrix/unit-test/test_io_gen.cpp
@@ -11,7 +11,6 @@ struct comp_io
 void run_test()
 {
 	size_t row_block_size = 1024;
-	size_t rb_io_size = 1024;
 	size_t num_rows = random() + 1000000000L;
 	size_t num_blocks = ceil(((double) num_rows) / row_block_size);
 	std::vector<row_block> blocks;
@@ -24,26 +23,37 @@ void run_test()
 
 	int num_gens = 32;
 	std::vector<matrix_io> ios;
+	std::vector<matrix_io_generator::ptr> io_gens(num_gens);
 	for (int i = 0; i < num_gens; i++) {
-		matrix_io_generator::ptr gen = matrix_io_generator::create(blocks,
+		io_gens[i] = matrix_io_generator::create(blocks,
 				num_rows, 1000, 0, i, num_gens);
-		while (gen->has_next_io())
-			ios.push_back(gen->get_next_io());
+	}
+	while (!io_gens.empty()) {
+		int idx = random() % io_gens.size();
+		if (io_gens[idx]->has_next_io()) {
+			if (random() % 10 == 0)
+				ios.push_back(io_gens[idx]->steal_io());
+			else
+				ios.push_back(io_gens[idx]->get_next_io());
+		}
+		else
+			io_gens.erase(io_gens.begin() + idx);
 	}
 	std::sort(ios.begin(), ios.end(), comp_io());
 	printf("There are %ld ios\n", ios.size());
 	off_t prev_off = ios[0].get_loc().get_offset();
 	size_t prev_size = ios[0].get_size();
 	off_t prev_row = ios[0].get_top_left().get_row_idx();
+	size_t prev_num_rows = ios[0].get_num_rows();
 	assert(prev_off == 0);
 	assert(prev_row == 0);
 	for (size_t i = 1; i < ios.size() - 1; i++) {
 		assert(ios[i].get_loc().get_offset() == prev_off + prev_size);
-		assert(ios[i].get_top_left().get_row_idx()
-				== prev_row + row_block_size * rb_io_size);
+		assert(ios[i].get_top_left().get_row_idx() == prev_row + prev_num_rows);
 		prev_off = ios[i].get_loc().get_offset();
 		prev_size = ios[i].get_size();
 		prev_row = ios[i].get_top_left().get_row_idx();
+		prev_num_rows = ios[i].get_num_rows();
 	}
 	assert(ios.back().get_loc().get_offset() + ios.back().get_size()
 			== blocks.back().get_offset());
