1b562453c30445d7f39778f857e64c38e91113d5
Da Zheng <zhengda1936@gmail.com>
2015-01-12 18:27:50 -0500

    [Matrix]: complete IRLM in R.

--- flash-graph/matrix/IRLM.R
+++ flash-graph/matrix/IRLM.R
@@ -48,11 +48,46 @@ lanczos <- function(A.multiply, k, m, r, V, T)
 			T[j+1, j] <- b
 		}
 	}
-	list(T, V, r)
+	list(T, V, r, b)
 }
 
-IRLM <- function(A.multiply, n, k, m)
- {
+compute.eigen <- function(T, which, last.beta, k, m, tol)
+{
+	ev <- eigen(T, TRUE)
+	if (which == "LA") {
+		ret <- sort(ev$values, decreasing=TRUE, index.return=TRUE)
+	}
+	else if (which == "SA") {
+		ret <- sort(ev$values, decreasing=FALSE, index.return=TRUE)
+	}
+	else if (which == "LM") {
+		ret <- sort(abs(ev$values), decreasing=TRUE, index.return=TRUE)
+	}
+	else if (which == "SM") {
+		ret <- sort(abs(ev$values), decreasing=FALSE, index.return=TRUE)
+	}
+	sorted.eigen.values <- ev$values[ret$ix]
+	sorted.eigen.vecs <- ev$vectors[,ret$ix]
+
+	m <- dim(sorted.eigen.vecs)[1]
+	converged <- c()
+	for (i in 1:k) {
+		bound <- abs(last.beta * sorted.eigen.vecs[m, i])
+		if (bound < tol * abs(sorted.eigen.values[i])) {
+			converged <- append(converged, i)
+		}
+	}
+
+	# wanted eigen values
+	list(wval=sorted.eigen.values[converged],
+		 # wanted eigen vectors
+		 wvec=sorted.eigen.vecs[, converged],
+		 # unwanted eigen values
+		 uval=sorted.eigen.values[(k+1):length(sorted.eigen.values)])
+}
+
+IRLM <- function(A.multiply, n, k, m, which, tol)
+{
 	p <- m - k
 #	n <- dim(A)[1]
 	V <- matrix(rep.int(0, n*m), nrow=n, ncol=m)
@@ -63,13 +98,17 @@ IRLM <- function(A.multiply, n, k, m)
 	T <- res[[1]]
 	V <- res[[2]]
 	rm <- res[[3]]
-	for (i in 1:5) {
-		ev <- eigen(T, TRUE, only.values=TRUE)
-		eigen.values <- sort(ev$values, decreasing=TRUE)
+	last.beta <- res[[4]]
+	while (TRUE) {
+		ev <- compute.eigen(T, which, last.beta, k, m, tol)
+		if (length(ev$wval) >= k) {
+			return(list(values=ev$wval[1:k], vectors=V %*% ev$wvec[, 1:k]))
+		}
+
 		Q <- diag(m)
 		for (j in 1:p) {
-			mu <- eigen.values[j + k]
-			qr.res <- qr(T - mu * diag(m))
+			mu <- ev$uval[j]
+			qr.res <- qr(T - mu * diag(m), tol=tol)
 			Qj <- qr.Q(qr.res)
 			T <- t(Qj) %*% T
 			T <- T %*% Qj
@@ -87,8 +126,8 @@ IRLM <- function(A.multiply, n, k, m)
 		T <- res[[1]]
 		V <- res[[2]]
 		rm <- res[[3]]
+		last.beta <- res[[4]]
 	}
-	eigen.values[1:k]
 }
 
 SVD <- function(A, k, m)
