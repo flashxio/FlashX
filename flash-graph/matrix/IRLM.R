cf2aeae791731217068ced77b5a0ae766bfc019e
Da Zheng <zhengda1936@gmail.com>
2015-01-14 21:30:24 -0500

    [Matrix]: rewrite Lanczos based on arpack source code 1.

--- flash-graph/matrix/IRLM.R
+++ flash-graph/matrix/IRLM.R
@@ -19,9 +19,30 @@
 
 initr <- function(n)
 {
-	x <- rep.int(1, n)
-#	x <- runif(n)
-	x <- x / norm(x, type="2")
+#	x <- rep.int(1, n)
+	x <- runif(n, -1, 1)
+}
+
+# In this version of initialization of r, we need to reorthoganize r against
+# the current Lanczos basis.
+reinitr <- function(n, V)
+{
+	r <- initr(n)
+	b0 <- norm(r, type="2")
+
+	iter <- 0
+	while (iter < 5) {
+		iter <- iter + 1
+		s <- t(V) %*% r
+		r <- r - V %*% s
+		b <- norm(r, type="2")
+		if (b <= 0.717 * b0)
+			break
+		b0 <- b
+		print("reothoganize r in reinitr")
+	}
+	stopifnot(iter < 5)
+	r
 }
 
 lanczos <- function(A.multiply, k, m, r, V, T)
@@ -32,7 +53,9 @@ lanczos <- function(A.multiply, k, m, r, V, T)
 		restart <- FALSE
 		# Step 1: we need to reinitialize r if necessary.
 		if (b == 0) {
-			r <- initr(n)
+			stopifnot(j > 1)
+			print("reinitialize r")
+			r <- reinitr(n, V[,1:(j-1)])
 			restart <- TRUE
 		}
 
@@ -75,15 +98,24 @@ lanczos <- function(A.multiply, k, m, r, V, T)
 				b <- new.b
 				break
 			}
-			else {
+			# Try to orthoganize r again
+			else if (orth.iter < 2) {
+				print("reothoganize r again")
 				b <- new.b
 				next
 			}
-			r <- 0
-			b <- 0
+			# If we still can't orthoganize r, we need to go to the first
+			# step and reinitialize r.
+			else {
+				print("fail to reorthoganize r")
+				r <- 0
+				b <- 0
+				break
+			}
 		}
 
 		if (j > 1 && T[j, j-1] < 0) {
+			print("negative T[j,j-1]")
 			T[j, j-1] <- -T[j, j-1]
 			T[j-1, j] <- -T[j-1, j]
 			if (j < m)
@@ -95,44 +127,62 @@ lanczos <- function(A.multiply, k, m, r, V, T)
 	list(T, V, r, b)
 }
 
-compute.eigen <- function(T, which, last.beta, k, m, tol)
+compute.eigen <- function(T, last.beta)
 {
+	m <- dim(T)[1]
 	ev <- eigen(T, TRUE)
+	bounds <- rep.int(0, m)
+	for (i in 1:m)
+		bounds[i] <- abs(last.beta * ev$vectors[m, i])
+	list(values=ev$values, vectors=ev$vectors, bounds=bounds)
+}
+
+sort.eigen <- function(ev, which, k)
+{
 	if (which == "LA") {
-		ret <- sort(ev$values, decreasing=TRUE, index.return=TRUE)
+		sort.ret <- sort(ev$values, decreasing=TRUE, index.return=TRUE)
 	}
 	else if (which == "SA") {
-		ret <- sort(ev$values, decreasing=FALSE, index.return=TRUE)
+		sort.ret <- sort(ev$values, decreasing=FALSE, index.return=TRUE)
 	}
 	else if (which == "LM") {
-		ret <- sort(abs(ev$values), decreasing=TRUE, index.return=TRUE)
+		sort.ret <- sort(abs(ev$values), decreasing=TRUE, index.return=TRUE)
 	}
 	else if (which == "SM") {
-		ret <- sort(abs(ev$values), decreasing=FALSE, index.return=TRUE)
+		sort.ret <- sort(abs(ev$values), decreasing=FALSE, index.return=TRUE)
 	}
-	sorted.eigen.values <- ev$values[ret$ix]
-	sorted.eigen.vecs <- ev$vectors[,ret$ix]
+	ret <- list(values=ev$values[sort.ret$ix], vectors=ev$vectors[,sort.ret$ix],
+		 bounds=ev$bounds[sort.ret$ix])
+	m <- length(ev$values)
+	if (m - k > 1) {
+		sort.ret <- sort(abs(ret$bounds[(k+1):m]), decreasing=TRUE, index.return=TRUE)
+		ret$values[(k+1):m] <- ret$values[k + sort.ret$ix]
+		ret$vectors[,(k+1):m] <- ret$vectors[,k + sort.ret$ix]
+		ret$bounds[(k+1):m] <- ret$bounds[k + sort.ret$ix]
+	}
+	ret
+}
 
-	m <- dim(sorted.eigen.vecs)[1]
+test.conv <- function(ev, k, tol)
+{
 	converged <- c()
 	for (i in 1:k) {
-		bound <- abs(last.beta * sorted.eigen.vecs[m, i])
-		if (bound < tol * abs(sorted.eigen.values[i])) {
+		if (ev$bounds[i] < tol * abs(ev$values[i])) {
 			converged <- append(converged, i)
 		}
 	}
 
+	m <- length(ev$values)
 	# wanted eigen values
-	list(wval=sorted.eigen.values[converged],
+	list(wval=ev$values[converged],
 		 # wanted eigen vectors
-		 wvec=sorted.eigen.vecs[, converged],
+		 wvec=ev$vectors[, converged],
 		 # unwanted eigen values
-		 uval=sorted.eigen.values[(k+1):length(sorted.eigen.values)])
+		 uval=ev$values[(k+1):m])
 }
 
 IRLM <- function(A.multiply, n, k, m, which, tol)
 {
-	p <- m - k
 #	n <- dim(A)[1]
 	V <- matrix(rep.int(0, n*m), nrow=n, ncol=m)
 	T <- matrix(rep.int(0, m*m), nrow=m, ncol=m)
@@ -142,30 +192,61 @@ IRLM <- function(A.multiply, n, k, m, which, tol)
 	V <- res[[2]]
 	rm <- res[[3]]
 	last.beta <- res[[4]]
+	iter <- 0
 	while (TRUE) {
-		ev <- compute.eigen(T, which, last.beta, k, m, tol)
-		if (length(ev$wval) >= k) {
-			return(list(values=ev$wval[1:k], vectors=V %*% ev$wvec[, 1:k]))
+		iter <- iter + 1
+
+		ev <- compute.eigen(T, last.beta)
+		ev <- sort.eigen(ev, which, k)
+		res <- test.conv(ev, k, tol)
+		nconv <- length(res$wval)
+		bounds <- ev$bounds
+		np <- m - k
+		nev <- k
+		for (i in (k+1):m) {
+			if (bounds[i] == 0) {
+				np <- np - 1
+				nev <- nev + 1
+			}
+		}
+		
+		if (nconv >= k || np == 0) {
+			return(list(values=res$wval[1:k], vectors=V %*% res$wvec[, 1:k]))
+		}
+
+		if (nconv < nev) {
+			nevbef <- nev
+			nev <- nev + min(nconv, np / 2)
+			if (nev == 1 && m >= 6)
+				nev <- m / 2
+			else if (nev == 1 && m > 2)
+				nev <- 2
+			np <- m - nev
+			if (nevbef < nev) {
+				ev <- sort.eigen(ev, which, nev)
+				print("sort eigen again")
+			}
+			print(nev)
 		}
 
 		Q <- diag(m)
-		for (j in 1:p) {
-			mu <- ev$uval[j]
+		for (j in 1:np) {
+			mu <- res$uval[j]
 			qr.res <- qr(T - mu * diag(m), tol=tol)
 			Qj <- qr.Q(qr.res)
-			T <- t(Qj) %*% T
-			T <- T %*% Qj
+			T <- t(Qj) %*% T %*% Qj
 			Q <- Q %*% Qj
 		}
-		bk <- T[k+1, k]
-		sigma <- Q[m, k]
-		rk <-  rm * sigma
-		V[, 1:k] <- V %*% Q[,1:k];
+		bk <- T[nev+1, nev]
+		sigma <- Q[m, nev]
+		rk <- bk * V[,nev+1] + rm * sigma
+#		rk <- rm * sigma
+		V[, 1:nev] <- V %*% Q[,1:nev];
 		tmp <- matrix(rep.int(0, m * m), nrow=m, ncol=m)
-		tmp[1:k,1:k] <- T[1:k,1:k]
+		tmp[1:nev,1:nev] <- T[1:nev,1:nev]
 		T <- tmp
 
-		res <- lanczos(A.multiply, k, m, rk, V, T)
+		res <- lanczos(A.multiply, nev, m, rk, V, T)
 		T <- res[[1]]
 		V <- res[[2]]
 		rm <- res[[3]]
