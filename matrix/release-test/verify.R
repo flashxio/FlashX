94caff1bcf6fe6ee5b62b1f1b74e4bb1e765e861
Da Zheng <zhengda1936@gmail.com>
2015-01-29 10:27:52 -0500

    [Matrix]: verify the output type against R.

--- matrix/release-test/verify.R
+++ matrix/release-test/verify.R
@@ -45,50 +45,62 @@ stopifnot(fm.ncol(fm.mat) == 100)
 mat1 <- fm.conv.FM2R(fm.mat)
 stopifnot(sum(mat1 == mat) == fm.ncol(fm.mat) * fm.nrow(fm.mat))
 
+test.MV1 <- function(fm.mat, fm.vec, res)
+{
+	fm.res <- fm.multiply(fm.mat, fm.vec)
+	stopifnot(sum(res[, 1] == fm.conv.FM2R(fm.res)) == fm.length(fm.res))
+	stopifnot(fm.is.vector(fm.res))
+	stopifnot(fm.length(fm.res) == fm.nrow(fm.mat))
+	# TODO
+#	stopifnot(fm.typeof(fm.res) == typeof(res))
+}
+
 # Test a dense matrix times a vector
-test.MV <- function(mat, vec, res.type)
+test.MV <- function(mat, vec)
 {
 	res <- mat %*% vec
-
+	cat("matrix:", typeof(mat), ", vector:", typeof(vec), ", res:", typeof(res), "\n")
 	# TODO
 	#print("column-wise matrix times a vector")
 	#fm.mat <- fm.conv.R2FM(mat, byrow = FALSE)
 	#fm.vec <- fm.conv.R2FM(vec)
 	#stopifnot(fm.matrix.layout(fm.mat) == "col")
-	#fm.res <- fm.multiply(fm.mat, fm.vec)
-	#stopifnot(sum(res[, 0] == fm.conv.FM2R(fm.res)) == fm.length(fm.res))
-	#stopifnot(fm.is.vector(res))
-	#stopifnot(fm.length(res) == fm.nrow(fm.mat))
-	#stopifnot(fm.typeof(res) == res.type)
+	#test.MV1(fm.mat, fm.vec, res)
 
 	print("Row-wise matrix times a vector")
 	fm.mat <- fm.conv.R2FM(mat, byrow = TRUE)
 	fm.vec <- fm.conv.R2FM(vec)
 	stopifnot(fm.matrix.layout(fm.mat) == "row")
-	fm.res <- fm.multiply(fm.mat, fm.vec)
-	stopifnot(sum(res[, 1] == fm.conv.FM2R(fm.res)) == fm.length(fm.res))
-	stopifnot(fm.is.vector(fm.res))
-	stopifnot(fm.length(fm.res) == fm.nrow(fm.mat))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	test.MV1(fm.mat, fm.vec, res)
 }
 
 mat <- matrix(1:2000, 20, 100)
 vec <- 1:ncol(mat)
-test.MV(mat, vec, "integer")
+test.MV(mat, vec)
 
 mat <- matrix(1:2000, 20, 100)
 vec <- seq.int(1, ncol(mat), 1)
-test.MV(mat, vec, "double")
+test.MV(mat, vec)
 
 mat <- matrix(seq.int(1, 2000, 1), 20, 100)
 vec <- 1:ncol(mat)
-test.MV(mat, vec, "double")
+test.MV(mat, vec)
 
 mat <- matrix(seq.int(1, 2000, 1), 20, 100)
 vec <- seq.int(1, ncol(mat), 1)
-test.MV(mat, vec, "double")
+test.MV(mat, vec)
 
-test.MM <- function(left.type, right.type, res.type)
+test.MM1 <- function(fm.left, fm.right, res)
+{
+	fm.res <- fm.multiply(fm.left, fm.right)
+	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == fm.nrow(fm.res) * fm.ncol(fm.res))
+	stopifnot(fm.nrow(fm.res) == fm.nrow(fm.left))
+	stopifnot(fm.ncol(fm.res) == fm.ncol(fm.right))
+	# TODO
+#	stopifnot(fm.typeof(fm.res) == typeof(res))
+}
+
+test.MM <- function(left.type, right.type)
 {
 	if (left.type == "integer")
 		left.mat <- matrix(1:2000, 20, 100)
@@ -101,8 +113,9 @@ test.MM <- function(left.type, right.type, res.type)
 
 	print("a wide dense matrix times a tall dense matrix")
 	res <- left.mat %*% right.mat
+	cat("left:", typeof(left.mat), ", right:", typeof(right.mat), ", res:", typeof(res), "\n")
 
-	test.MM1 <- function(left.byrow, right.byrow)
+	test.MM.tmp <- function(left.byrow, right.byrow)
 	{
 		cat("left:", nrow(left.mat), ncol(left.mat), ", right:",
 			nrow(right.mat), ncol(right.mat), "\n")
@@ -118,24 +131,20 @@ test.MM <- function(left.type, right.type, res.type)
 		stopifnot(fm.matrix.layout(fm.left) == left.layout)
 		fm.right <- fm.conv.R2FM(right.mat, byrow = right.byrow)
 		stopifnot(fm.matrix.layout(fm.right) == right.layout)
-		fm.res <- fm.multiply(fm.left, fm.right)
-		stopifnot(sum(res == fm.conv.FM2R(fm.res)) == fm.nrow(fm.res) * fm.ncol(fm.res))
-		stopifnot(fm.nrow(fm.res) == fm.nrow(fm.left))
-		stopifnot(fm.ncol(fm.res) == fm.ncol(fm.right))
-		stopifnot(fm.typeof(fm.res) == res.type)
+		test.MM1(fm.left, fm.right, res)
 	}
 
 	#print("Column-wise matrix times a column-wise matrix")
-	#test.MM1(FALSE, FALSE)
+	#test.MM.tmp(FALSE, FALSE)
 
 	#print("Column-wise matrix times a row-wise matrix")
-	#test.MM1(FALSE, TRUE)
+	#test.MM.tmp(FALSE, TRUE)
 
 	print("Row-wise matrix times a column-wise matrix")
-	test.MM1(TRUE, FALSE)
+	test.MM.tmp(TRUE, FALSE)
 
 	#print("Row-wise matrix times a row-wise matrix")
-	#test.MM1(TRUE, TRUE)
+	#test.MM.tmp(TRUE, TRUE)
 
 	print("a tall dense matrix times a wide dense matrix")
 	if (left.type == "integer")
@@ -149,151 +158,161 @@ test.MM <- function(left.type, right.type, res.type)
 	res <- left.mat %*% right.mat
 
 	print("Column-wise matrix times a column-wise matrix")
-	test.MM1(FALSE, FALSE)
+	test.MM.tmp(FALSE, FALSE)
 
 	print("Column-wise matrix times a row-wise matrix")
-	test.MM1(FALSE, TRUE)
+	test.MM.tmp(FALSE, TRUE)
 
 	print("Row-wise matrix times a column-wise matrix")
-	test.MM1(TRUE, FALSE)
+	test.MM.tmp(TRUE, FALSE)
 
 	#print("Row-wise matrix times a row-wise matrix")
-	#test.MM1(TRUE, TRUE)
+	#test.MM.tmp(TRUE, TRUE)
 }
 
-test.MM("integer", "integer", "integer")
-test.MM("integer", "double", "double")
-test.MM("double", "integer", "double")
-test.MM("double", "double", "double")
+test.MM("integer", "integer")
+test.MM("integer", "double")
+test.MM("double", "integer")
+test.MM("double", "double")
 
-test.basic.op1 <- function(fm.o1, fm.o2, ro1, ro2, res.type)
+test.basic.op1 <- function(fm.o1, fm.o2, ro1, ro2)
 {
 	fm.res <- fm.o1 + fm.o2
 	res <- ro1 + ro2
+	cat("add: left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 - fm.o2
 	res <- ro1 - ro2
+	cat("sub left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 * fm.o2
 	res <- ro1 * ro2
+	cat("mul left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 / fm.o2
 	res <- ro1 / ro2
+	cat("div left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == "double")
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 }
 
-test.basic.op <- function(fm.o1, fm.o2, res.type)
+test.basic.op <- function(fm.o1, fm.o2)
 {
 	ro1 <- fm.conv.FM2R(fm.o1)
 	ro2 <- fm.conv.FM2R(fm.o2)
 
 	fm.res <- fm.o1 + fm.o2
 	res <- ro1 + ro2
+	cat("add left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 - fm.o2
 	res <- ro1 - ro2
+	cat("sub left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 * fm.o2
 	res <- ro1 * ro2
+	cat("mul left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.o1 / fm.o2
 	res <- ro1 / ro2
+	cat("div left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == "double")
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.pmin2(fm.o1, fm.o2)
 	res <- pmin(ro1, ro2)
+	cat("min left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 
 	fm.res <- fm.pmax2(fm.o1, fm.o2)
 	res <- pmax(ro1, ro2)
+	cat("max left:", typeof(ro1), ", right:", typeof(ro2), ", res:", typeof(res), "\n")
 	stopifnot(sum(res == fm.conv.FM2R(fm.res)) == length(res))
-	stopifnot(fm.typeof(fm.res) == res.type)
+	stopifnot(fm.typeof(fm.res) == typeof(res))
 }
 
 print("pair-wise integer-integer vector")
 fm.vec1 <- fm.conv.R2FM(1:2000)
 fm.vec2 <- fm.conv.R2FM(1:2000)
-test.basic.op(fm.vec1, fm.vec2, "integer")
+test.basic.op(fm.vec1, fm.vec2)
 
 print("pair-wise double-integer vector")
 fm.vec1 <- fm.seq.int(1, 2000, 1)
 fm.vec2 <- fm.conv.R2FM(1:2000)
-test.basic.op(fm.vec1, fm.vec2, "double")
+test.basic.op(fm.vec1, fm.vec2)
 
 print("pair-wise integer-double vector")
 fm.vec1 <- fm.conv.R2FM(1:2000)
 fm.vec2 <- fm.seq.int(1, 2000, 1)
-test.basic.op(fm.vec1, fm.vec2, "double")
+test.basic.op(fm.vec1, fm.vec2)
 
 print("pair-wise double-double vector")
 fm.vec1 <- fm.seq.int(1, 2000, 1)
 fm.vec2 <- fm.seq.int(1, 2000, 1)
-test.basic.op(fm.vec1, fm.vec2, "double")
+test.basic.op(fm.vec1, fm.vec2)
 
 print("pair-wise integer-integer matrix")
 fm.mat1 <- fm.matrix(fm.conv.R2FM(1:2000), 20, 100)
 fm.mat2 <- fm.matrix(fm.conv.R2FM(1:2000), 20, 100)
-test.basic.op(fm.mat1, fm.mat2, "integer")
+test.basic.op(fm.mat1, fm.mat2)
 
 print("pair-wise double-integer matrix")
 fm.mat1 <- fm.matrix(fm.seq.int(1, 2000, 1), 20, 100)
 fm.mat2 <- fm.matrix(fm.conv.R2FM(1:2000), 20, 100)
-test.basic.op(fm.mat1, fm.mat2, "double")
+test.basic.op(fm.mat1, fm.mat2)
 
 print("pair-wise integer-double matrix")
 fm.mat1 <- fm.matrix(fm.conv.R2FM(1:2000), 20, 100)
 fm.mat2 <- fm.matrix(fm.seq.int(1, 2000, 1), 20, 100)
-test.basic.op(fm.mat1, fm.mat2, "double")
+test.basic.op(fm.mat1, fm.mat2)
 
 print("pair-wise double-double matrix")
 fm.mat1 <- fm.matrix(fm.seq.int(1, 2000, 1), 20, 100)
 fm.mat2 <- fm.matrix(fm.seq.int(1, 2000, 1), 20, 100)
-test.basic.op(fm.mat1, fm.mat2, "double")
+test.basic.op(fm.mat1, fm.mat2)
 
 int.v <- (1:3)[1]
 
 fm.vec <- fm.conv.R2FM(1:2000)
 rvec <- fm.conv.FM2R(fm.vec)
 print("vector-element integer-integer")
-test.basic.op1(fm.vec, int.v, rvec, int.v, "integer")
+test.basic.op1(fm.vec, int.v, rvec, int.v)
 print("vector-element integer-double")
-test.basic.op1(fm.vec, 1.0, rvec, 1.0, "double")
+test.basic.op1(fm.vec, 1.0, rvec, 1.0)
 
 fm.vec <- fm.seq.int(1, 2000, 1)
 rvec <- fm.conv.FM2R(fm.vec)
 print("vector-element double-int")
-test.basic.op1(fm.vec, int.v, rvec, int.v, "double")
+test.basic.op1(fm.vec, int.v, rvec, int.v)
 print("vector-element double-double")
-test.basic.op1(fm.vec, 1.0, rvec, 1.0, "double")
+test.basic.op1(fm.vec, 1.0, rvec, 1.0)
 
 fm.vec <- fm.conv.R2FM(1:2000)
 rvec <- fm.conv.FM2R(fm.vec)
 print("element-vector integer-integer")
-test.basic.op1(int.v, fm.vec, int.v, rvec, "integer")
+test.basic.op1(int.v, fm.vec, int.v, rvec)
 print("element-vector integer-double")
-test.basic.op1(1.0, fm.vec, 1.0, rvec, "double")
+test.basic.op1(1.0, fm.vec, 1.0, rvec)
 
 fm.vec <- fm.seq.int(1, 2000, 1)
 rvec <- fm.conv.FM2R(fm.vec)
 print("element-vector double-int")
-test.basic.op1(int.v, fm.vec, int.v, rvec, "double")
+test.basic.op1(int.v, fm.vec, int.v, rvec)
 print("element-vector double-double")
-test.basic.op1(1.0, fm.vec, 1.0, rvec, "double")
+test.basic.op1(1.0, fm.vec, 1.0, rvec)
 
 print("Transpose a matrix")
 fm.mat <- fm.matrix(fm.seq.int(1, 2000, 1), 20, 100)
