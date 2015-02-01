1c52d7f62a43412c49e4541ec99165b2b416e745
Da Zheng <zhengda1936@gmail.com>
2015-01-31 22:07:38 -0500

    [R]: move pure R functions in FlashR to another R file.

---
+++ Rpkg/R/flashmatrix_ext.R
@@ -0,0 +1,86 @@
+# Copyright 2014 Open Connectome Project (http://openconnecto.me)
+# Written by Da Zheng (zhengda1936@gmail.com)
+#
+# This file is part of FlashR.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file contains the FlashR functions implemented with
+# the FlashR functions in flashmatrix.R.
+
+#' Maxima and Minima
+#'
+#' Returns the (parallel) maxima and minima of the input values.
+#'
+#' `fm.pmin2' and `fm.pmax2' works on two input vectors and returns
+#' return a single vector/matrix giving the 'parallel' maxima (or minima) of
+#' the vectors/matrices. The first element of the result is the maximum (minimum)
+#' of the first elements of all the arguments, the second element of
+#' the result is the maximum (minimum) of the second elements of all
+#' the arguments and so on.
+#'
+#' @param o1 the input vector/matrix.
+#' @param o2 the input vector/matrix.
+#' @return a vector/matrix.
+#' @name minmax
+#' @author Da Zheng <dzheng5@@jhu.edu>
+fm.pmin2 <- function(o1, o2)
+{
+	fm.mapply2(fm.bo.min, o1, o2)
+}
+
+#' @name minmax
+fm.pmax2 <- function(o1, o2)
+{
+	fm.mapply2(fm.bo.max, o1, o2)
+}
+
+`+.fm` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.add, o1, o2)
+}
+
+`+.fmV` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.add, o1, o2)
+}
+
+`-.fm` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.sub, o1, o2)
+}
+
+`-.fmV` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.sub, o1, o2)
+}
+
+`*.fm` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.mul, o1, o2)
+}
+
+`*.fmV` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.mul, o1, o2)
+}
+
+`/.fm` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.div, o1, o2)
+}
+
+`/.fmV` <- function(o1, o2)
+{
+	fm.ele.wise.op(fm.bo.div, o1, o2)
+}
