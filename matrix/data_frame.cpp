349cff7e516181be13c8d70369d6e75581434ade
Da Zheng <zhengda1936@gmail.com>
2015-02-24 10:09:49 -0500

    [Matrix]: add data frame.

---
+++ matrix/data_frame.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Open Connectome Project (http://openconnecto.me)
+ * Written by Da Zheng (zhengda1936@gmail.com)
+ *
+ * This file is part of FlashMatrix.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "log.h"
+
+#include "data_frame.h"
+
+namespace fm
+{
+
+bool data_frame::append(std::vector<data_frame::ptr>::const_iterator begin,
+		std::vector<data_frame::ptr>::const_iterator end)
+{
+	std::unordered_map<std::string, std::vector<vector::ptr> > vecs;
+	for (size_t i = 0; i < named_vecs.size(); i++) {
+		std::vector<vector::ptr> _vecs;
+		_vecs.push_back(get_vec(named_vecs[i].first));
+		vecs.insert(std::pair<std::string, std::vector<vector::ptr> >(
+					named_vecs[i].first, _vecs));
+	}
+
+	for (auto it = begin; it != end; it++) {
+		data_frame::ptr df = *it;
+		if (df->get_num_vecs() != get_num_vecs()) {
+			BOOST_LOG_TRIVIAL(error)
+				<< "The data frames have different numbers of vectors";
+			return false;
+		}
+		for (auto vec_it = vecs.begin(); vec_it != vecs.end(); vec_it++) {
+			vector::ptr vec = df->get_vec(vec_it->first);
+			if (vec == NULL) {
+				BOOST_LOG_TRIVIAL(error)
+					<< "The data frames have different names on the vectors";
+				return false;
+			}
+			if (vec->get_type() != vec_it->second.front()->get_type()) {
+				BOOST_LOG_TRIVIAL(error)
+					<< "The data frames have different types for the vectors with the same name";
+				return false;
+			}
+			vec_it->second.push_back(vec);
+		}
+	}
+
+	for (auto it = vecs.begin(); it != vecs.end(); it++)
+		it->second.front()->append(it->second.begin() + 1, it->second.end());
+	return true;
+}
+
+}
---
+++ matrix/data_frame.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014 Open Connectome Project (http://openconnecto.me)
+ * Written by Da Zheng (zhengda1936@gmail.com)
+ *
+ * This file is part of FlashMatrix.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __DATA_FRAME_H__
+#define __DATA_FRAME_H__
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+#include "vector.h"
+
+namespace fm
+{
+
+typedef std::pair<std::string, vector::ptr> named_vec_t;
+
+/**
+ * This implements the data frame in R.
+ * It also resembles a SQL table. Data is stored column-wise and each column
+ * has its own data type
+ */
+class data_frame
+{
+	std::vector<named_vec_t> named_vecs;
+	std::unordered_map<std::string, vector::ptr> vec_map;
+
+	data_frame() {
+	}
+
+	data_frame(const std::vector<named_vec_t> &named_vecs) {
+		this->named_vecs = named_vecs;
+		for (auto it = named_vecs.begin(); it != named_vecs.end(); it++)
+			vec_map.insert(*it);
+	}
+public:
+	typedef std::shared_ptr<data_frame> ptr;
+
+	static ptr create() {
+		return ptr(new data_frame());
+	}
+
+	static ptr create(const std::vector<named_vec_t> &named_vecs) {
+		return ptr(new data_frame(named_vecs));
+	}
+
+	bool add_vec(const std::string &name, vector::ptr vec) {
+		if (get_num_vecs() > 0 && vec->get_length() != get_num_entries())
+			return false;
+		named_vecs.push_back(named_vec_t(name, vec));
+		vec_map.insert(named_vec_t(name, vec));
+		return true;
+	}
+
+	bool append(std::vector<data_frame::ptr>::const_iterator begin,
+			std::vector<data_frame::ptr>::const_iterator end);
+
+	vector::ptr get_vec(const std::string &name) const {
+		auto it = vec_map.find(name);
+		if (it == vec_map.end())
+			return vector::ptr();
+		else
+			return it->second;
+	}
+
+	size_t get_num_vecs() const {
+		return named_vecs.size();
+	}
+
+	size_t get_num_entries() const {
+		return named_vecs[0].second->get_length();
+	}
+};
+
+}
+
+#endif
