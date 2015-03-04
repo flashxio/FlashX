2e1ec7a8e89fa919f91e686253367e4aa8b3f8eb
Da Zheng <zhengda1936@gmail.com>
2015-03-04 00:21:06 -0500

    [Matrix]: one more test for groupby on data frame.

--- matrix/unit-test/test-data_frame.cpp
+++ matrix/unit-test/test-data_frame.cpp
@@ -39,6 +39,38 @@ void sum_apply_operate::run(const void *key, const data_frame &val,
 	t_out.set(0, sum);
 }
 
+class copy_apply_operate: public gr_apply_operate<data_frame>
+{
+public:
+	void run(const void *key, const data_frame &val, mem_vector &out) const;
+
+	const scalar_type &get_key_type() const {
+		static scalar_type_impl<int> t;
+		return t;
+	}
+
+	const scalar_type &get_output_type() const {
+		static scalar_type_impl<int> t;
+		return t;
+	}
+
+	size_t get_num_out_eles() const {
+		return 1;
+	}
+};
+
+void copy_apply_operate::run(const void *key, const data_frame &val,
+		mem_vector &out) const
+{
+	assert(val.get_num_vecs() == 2);
+	assert(out.is_type<int>());
+	type_mem_vector<int>::ptr vec = type_mem_vector<int>::cast(val.get_vec(1));
+	type_mem_vector<int> &t_out = (type_mem_vector<int> &) out;
+	out.resize(vec->get_length());
+	for (size_t i = 0; i < vec->get_length(); i++)
+		t_out.set(i, vec->get(i));
+}
+
 void test_groupby()
 {
 	mem_data_frame::ptr df = mem_data_frame::create();
@@ -61,17 +93,22 @@ void test_groupby()
 		else
 			it->second += v2;
 	}
-
 	df->add_vec("vec1", vec1);
 	df->add_vec("vec2", vec2);
-	sum_apply_operate op;
-	vector_vector::ptr vv = df->groupby("vec1", op);
+
+	sum_apply_operate sum_op;
+	vector_vector::ptr vv = df->groupby("vec1", sum_op);
 	type_mem_vector<long>::ptr v = type_mem_vector<long>::cast(vv->cat());
 	assert(map.size() == v->get_length());
 	size_t idx = 0;
 	for (auto it = map.begin(); it != map.end(); it++, idx++) {
 		assert(it->second == v->get(idx));
 	}
+
+	copy_apply_operate copy_op;
+	vv = df->groupby("vec1", copy_op);
+	type_mem_vector<int>::ptr v1 = type_mem_vector<int>::cast(vv->cat());
+	assert(vec1->get_length() == v1->get_length());
 }
 
 int main()
