8e5d5d2a9fab65ddec2911d4018720c4613164ea
Da Zheng <zhengda1936@gmail.com>
2015-03-24 11:43:33 -0400

    [Matrix]: improve release test.

--- matrix/release-test/run_release_test.sh
+++ matrix/release-test/run_release_test.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+echo "unit test on data frame"
 ../unit-test/test-data_frame
+echo "unit test on external-memory matrix"
 ../unit-test/test-EM_matrix
+echo "unit test on external-memory vector"
 ../unit-test/test-EM_vector
+echo "unit test on in-memory matrix"
 ../unit-test/test-mem_matrix
+echo "unit test on in-memory vector"
 ../unit-test/test-mem_vector
+echo "unit test on in-memory vector of vectors"
 ../unit-test/test-mem_vector_vector
+echo "unit test on sparse matrix"
 ../unit-test/test-sparse_matrix
 
+rm -f wiki-Vote.txt.gz
 wget http://snap.stanford.edu/data/wiki-Vote.txt.gz
 gunzip wiki-Vote.txt.gz
 sed '/^#/d' wiki-Vote.txt > wiki-Vote1.txt
