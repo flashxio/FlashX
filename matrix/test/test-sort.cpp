633675270f9cf62199aa131a074de05142acc37c
Da Zheng <zhengda1936@gmail.com>
2015-03-28 16:34:12 -0400

    [Matrix]: measure sort performance in NUMA_vector.

---
+++ matrix/test/test-sort.cpp
@@ -0,0 +1,57 @@
+#include "NUMA_vector.h"
+#include "matrix_config.h"
+#include "mem_vector.h"
+#include "mem_worker_thread.h"
+
+const size_t num_eles = 1024 * 1024 * 1024;
+
+using namespace fm;
+
+int num_nodes = 1;
+int nthreads = 8;
+
+class rand_set: public type_set_operate<long>
+{
+public:
+	void set(long *arr, size_t num_eles, off_t row_idx, off_t col_idx) const {
+		for (size_t i = 0; i < num_eles; i++)
+			arr[i] = random();
+	}
+};
+
+void test_sort()
+{
+	size_t len = (random() % num_eles) + num_eles;
+	mem_vector::ptr mem_vec = mem_vector::create(len, get_scalar_type<long>());
+	mem_vec->set_data(rand_set());
+	NUMA_vector::ptr numa_vec = NUMA_vector::create(len, num_nodes,
+			get_scalar_type<long>());
+	numa_vec->copy_from(mem_vec->get_raw_arr(), mem_vec->get_length() * sizeof(long));
+
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+	numa_vec->sort();
+	gettimeofday(&end, NULL);
+	printf("sort %ld elements in NUMA vector takes %.3f\n", numa_vec->get_length(),
+			time_diff(start, end));
+
+	gettimeofday(&start, NULL);
+	mem_vec->sort();
+	gettimeofday(&end, NULL);
+	printf("sort %ld elements in mem vector takes %.3f\n", numa_vec->get_length(),
+			time_diff(start, end));
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 3) {
+		num_nodes = atoi(argv[1]);
+		nthreads = atoi(argv[2]);
+	}
+
+	matrix_conf.set_num_nodes(num_nodes);
+	matrix_conf.set_num_threads(nthreads);
+	detail::mem_thread_pool::init_global_mem_threads(num_nodes,
+			nthreads / num_nodes);
+	test_sort();
+}
