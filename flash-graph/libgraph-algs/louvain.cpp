5e216e48288c70afcad421326d7c7b3ffeaaf1ef
Disa Mhembere <dmhembe1@jhu.edu>
2015-06-16 01:16:37 -0400

    [graph]: Louvain stub added

---
+++ flash-graph/libgraph-algs/louvain.cpp
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2014 Open Connectome Project (http://openconnecto.me)
+ * Written by Disa Mhembere (disa@jhu.edu)
+ *
+ * This file is part of FlashGraph.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifdef PROFILER
+#include <gperftools/profiler.h>
+#endif
+
+#include <limits>
+#include <vector>
+
+#include "thread.h"
+#include "io_interface.h"
+#include "container.h"
+#include "concurrency.h"
+
+#include "vertex_index.h"
+#include "graph_engine.h"
+#include "graph_config.h"
+#include "FGlib.h"
+#include "FG_vector.h"
+#include "save_result.h"
+
+using namespace fg;
+
+namespace {
+uint64_t m; // 1/2 the sum of all in and out edges
+
+class louvain_vertex: public compute_vertex
+{
+	float k; // vertex edge sum of in & out edges
+	vertex_id_t c; // current cluster
+	int delta; // This is per cluster but can be treated as per vertex if we rebuild graph
+	// TODO: Add more
+
+	public:
+	louvain_vertex(vertex_id_t id): compute_vertex(id) {
+		k = 0;
+		c = id; 
+		delta = 0;
+	}
+
+	void run(vertex_program &prog) {}
+	void run(vertex_program &prog, const page_vertex &vertex) {}
+	void run_on_message(vertex_program &, const vertex_message &msg1) {}
+};
+}
+
+namespace fg 
+{
+	void compute_louvain(FG_graph::ptr fg, const uint32_t levels)
+	{
+		graph_index::ptr index = NUMA_graph_index<louvain_vertex>::create(
+				fg->get_graph_header());
+		graph_engine::ptr graph = fg->create_engine(index);
+
+		BOOST_LOG_TRIVIAL(info) << "Starting Louvain with " << levels << " levels";
+		BOOST_LOG_TRIVIAL(info) << "prof_file: " << graph_conf.get_prof_file().c_str();
+#ifdef PROFILER
+		if (!graph_conf.get_prof_file().empty())
+			ProfilerStart(graph_conf.get_prof_file().c_str());
+#endif
+
+		struct timeval start, end;
+		gettimeofday(&start, NULL);
+
+		graph->init_all_vertices(vertex_initializer::ptr());
+
+		graph->start_all();
+		graph->wait4complete();
+
+		gettimeofday(&end, NULL);
+
+#ifdef PROFILER
+		if (!graph_conf.get_prof_file().empty())
+			ProfilerStop();
+#endif
+
+		BOOST_LOG_TRIVIAL(info) << boost::format("It takes %1% seconds")
+			% time_diff(start, end);
+
+		return;
+	}
+}
