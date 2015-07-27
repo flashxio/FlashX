ab0201da742d502d115554b75ff8c930e2487de0
Da Zheng <zhengda1936@gmail.com>
2015-07-27 14:26:53 -0400

    [R]: update FlashGraphR installation script.

--- install_FlashGraphR.sh
+++ install_FlashGraphR.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 R CMD build Rpkg
+version=`R --version | grep "R version" | awk '{print $3}' | awk -F. '{print $1 "." $2}'`
 if [ -d ~/R ]; then
-	path=`find ~/R -name Rcpp.h`
+	path=`find ~/R -name Rcpp.h | grep ${version}`
 fi
 if [ -z "$path" ]; then
 	path=`find /usr/local -name Rcpp.h`
