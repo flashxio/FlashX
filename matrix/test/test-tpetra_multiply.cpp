0e36eb8ce2ba9fd975a6b3fa8d66fe0d02a9c26c
Da Zheng <zhengda1936@gmail.com>
2015-08-06 23:56:11 -0400

    [Matrix]: enable MPI in testing SpMM and Anasazi eigensolvers.

--- matrix/test/test-tpetra_multiply.cpp
+++ matrix/test/test-tpetra_multiply.cpp
@@ -18,8 +18,8 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-typedef fg::vsize_t local_ordinal_type;
-typedef fg::vsize_t global_ordinal_type;
+typedef size_t local_ordinal_type;
+typedef size_t global_ordinal_type;
 
 typedef Tpetra::DefaultPlatform::DefaultPlatformType Platform;
 typedef Tpetra::DefaultPlatform::DefaultPlatformType::NodeType  Node;
@@ -29,56 +29,50 @@ typedef Tpetra::CrsMatrix<double, local_ordinal_type, global_ordinal_type> crs_m
 size_t edge_data_size = 0;
 
 ArrayRCP<size_t> getNumEntriesPerRow(fg::vertex_index::ptr index,
-		size_t &num_rows)
+		size_t first_row, size_t num_local_rows)
 {
 	fg::in_mem_query_vertex_index::ptr query_index
 		= fg::in_mem_query_vertex_index::create(index, false);
 
-	ArrayRCP<size_t> numEntries(index->get_num_vertices());
-	for (size_t i = 0; i < index->get_num_vertices(); i++)
-		numEntries[i] = query_index->get_num_edges(i, fg::edge_type::IN_EDGE);
-
-	num_rows = index->get_num_vertices();
+	ArrayRCP<size_t> numEntries(num_local_rows);
+	for (size_t i = 0; i < num_local_rows; i++)
+		numEntries[i] = query_index->get_num_edges(i + first_row,
+				fg::edge_type::IN_EDGE);
 	return numEntries;
 }
 
 RCP<crs_matrix_type> create_crs(fg::in_mem_graph::ptr g,
-		fg::vertex_index::ptr index, RCP<map_type> map)
+		fg::vertex_index::ptr index, RCP<map_type> map, int my_rank)
 {
-	safs::io_interface::ptr io = create_io(g->create_io_factory(),
-			thread::get_curr_thread());
+	std::shared_ptr<const char> graph_data = g->get_raw_data();
 	const size_t numMyElements = map->getNodeNumElements ();
+	const global_ordinal_type gblRow0 = map->getGlobalElement(0);
+	const size_t numRows = index->get_num_vertices();
 
 	printf("Get #entries per row\n");
-	size_t numRows;
-	ArrayRCP<const size_t> numEntriesPerRow = getNumEntriesPerRow(index, numRows);
-	assert(numRows == numMyElements);
+	ArrayRCP<const size_t> numEntriesPerRow = getNumEntriesPerRow(index,
+			gblRow0, numMyElements);
 	printf("allocate CRS matrix\n");
 	// Create a Tpetra sparse matrix whose rows have distribution given by the Map.
 	RCP<crs_matrix_type> A (new crs_matrix_type (map, numEntriesPerRow,
 				Tpetra::ProfileType::StaticProfile));
 
 	printf("fill the CRS matrix\n");
-	const size_t vheader_size = fg::ext_mem_undirected_vertex::get_header_size();
-	char vheader_buf[vheader_size];
-	const fg::ext_mem_undirected_vertex *vheader
-		= (const fg::ext_mem_undirected_vertex *) vheader_buf;
-	off_t off = fg::graph_header::get_header_size();
+	fg::in_mem_cundirected_vertex_index::ptr cu_vindex
+		= fg::in_mem_cundirected_vertex_index::create(*index);
+	printf("first vertex in process %d: %ld\n", my_rank, gblRow0);
+	off_t off = cu_vindex->get_vertex(gblRow0).get_off();
+	printf("The offset of the first vertex in the image is %ld\n", off);
 	std::vector<global_ordinal_type> cols;
 	std::vector<double> vals;
 	// Fill the sparse matrix, one row at a time.
 	for (local_ordinal_type lclRow = 0;
 			lclRow < static_cast<local_ordinal_type> (numMyElements); ++lclRow) {
-		io->access(vheader_buf, off, vheader_size, READ);
-		fg::vsize_t num_edges = vheader->get_num_edges();
-		assert(num_edges > 0);
-		size_t size = fg::ext_mem_undirected_vertex::num_edges2vsize(num_edges,
-				edge_data_size);
-		std::unique_ptr<char[]> buf(new char[size]);
-		io->access(buf.get(), off, size, READ);
-
+		const global_ordinal_type gblRow = map->getGlobalElement (lclRow);
 		const fg::ext_mem_undirected_vertex *v
-			= (const fg::ext_mem_undirected_vertex *) buf.get();
+			= (const fg::ext_mem_undirected_vertex *) (graph_data.get() + off);
+		assert(v->get_id() == gblRow);
+		fg::vsize_t num_edges = v->get_num_edges();
 		cols.resize(num_edges);
 		vals.resize(num_edges);
 		for (fg::vsize_t i = 0; i < num_edges; i++) {
@@ -87,10 +81,10 @@ RCP<crs_matrix_type> create_crs(fg::in_mem_graph::ptr g,
 			vals[i] = 1;
 		}
 		assert(num_edges == numEntriesPerRow[lclRow]);
-		const global_ordinal_type gblRow = map->getGlobalElement (lclRow);
 		assert(gblRow < numRows);
 		A->insertGlobalValues (gblRow, cols, vals);
-		off += size;
+		off += fg::ext_mem_undirected_vertex::num_edges2vsize(num_edges,
+				edge_data_size);
 	}
 
 	// Tell the sparse matrix that we are done adding entries to it.
@@ -98,19 +92,20 @@ RCP<crs_matrix_type> create_crs(fg::in_mem_graph::ptr g,
 	return A;
 }
 
-void test_tpetra(fg::FG_graph::ptr fg)
+void test_tpetra(fg::in_mem_graph::ptr g, fg::vertex_index::ptr index)
 {
 	typedef Tpetra::MultiVector<double, global_ordinal_type, global_ordinal_type, Node> MV;
 
 	Platform &platform = Tpetra::DefaultPlatform::getDefaultPlatform();
 	RCP<const Teuchos::Comm<int> > comm = platform.getComm();
+	const int myRank = comm->getRank ();
 	RCP<map_type> Map = rcp (new map_type(
-				fg->get_graph_header().get_num_vertices(), 0, comm));
+				index->get_graph_header().get_num_vertices(), 0, comm));
 
 	printf("start to convert FG format to CSR format\n");
 	struct timeval start, end;
 	gettimeofday(&start, NULL);
-	RCP<crs_matrix_type> A = create_crs(fg->get_graph_data(), fg->get_index_data(), Map);
+	RCP<crs_matrix_type> A = create_crs(g, index, Map, myRank);
 	gettimeofday(&end, NULL);
 	printf("conversion takes %.3f seconds\n", time_diff(start, end));
 
@@ -131,18 +126,21 @@ void test_tpetra(fg::FG_graph::ptr fg)
 
 int main(int argc, char *argv[])
 {
-	if (argc < 4) {
-		fprintf(stderr, "eigensolver conf_file graph_file index_file\n");
+	if (argc < 3) {
+		fprintf(stderr, "multiply graph_file index_file\n");
 		exit(1);
 	}
 
-	std::string conf_file = argv[1];
-	std::string graph_file = argv[2];
-	std::string index_file = argv[3];
+	std::string graph_file = argv[1];
+	std::string index_file = argv[2];
+	fg::in_mem_graph::ptr g = fg::in_mem_graph::load_graph(graph_file);
+	fg::vertex_index::ptr index = fg::vertex_index::load(index_file);
 
-	config_map::ptr configs = config_map::create(conf_file);
-	init_flash_matrix(configs);
+	Teuchos::GlobalMPISession mpiSession (&argc, &argv);
+	printf("MPI initialized: %d, #procs: %d, rank: %d\n",
+			Teuchos::GlobalMPISession::mpiIsInitialized(),
+			Teuchos::GlobalMPISession::getNProc(),
+			Teuchos::GlobalMPISession::getRank());
 
-	fg::FG_graph::ptr fg = fg::FG_graph::create(graph_file, index_file, configs);
-	test_tpetra(fg);
+	test_tpetra(g, index);
 }
