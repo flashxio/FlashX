e4728569dc7c046e9b66c7c07c6fa271d99b8bca
Disa Mhembere <dmhembe1@jhu.edu>
2015-09-25 01:59:55 -0400

    Added random init

--- flash-graph/libgraph-algs/sem_kmeans.cpp
+++ flash-graph/libgraph-algs/sem_kmeans.cpp
@@ -28,8 +28,10 @@
 #include "graph_engine.h"
 #include "graph_config.h"
 #include "FGlib.h"
+#include "save_result.h"
 
-#define KM_TEST 0
+#define KM_TEST 1
+#define INVALID_CLUST_ID -1
 
 using namespace fg;
 
@@ -45,16 +47,7 @@ namespace {
     enum dist_type_t { EUCL, COS }; // Euclidean, Cosine distance
 
     template <typename T>
-        static void print_arr(T* arr, unsigned len) {
-            printf("[ ");
-            for (unsigned i = 0; i < len; i++) {
-                std::cout << arr[i] << " ";
-            }
-            printf("]\n");
-        }
-
-    template <typename T>
-        static void print_vector(typename std::vector<T>& v) {
+        static void print_vector(typename std::vector<T> v) {
             std::cout << "[";
             typename std::vector<T>::iterator itr = v.begin();
             for (; itr != v.end(); itr++) {
@@ -63,18 +56,13 @@ namespace {
             std::cout <<  " ]\n";
         }
 
-    template <typename T>
-        static void print_mat(T* matrix, const unsigned rows, const unsigned cols) {
-            for (unsigned row = 0; row < rows; row++) {
-                std::cout << "[";
-                for (unsigned col = 0; col < cols; col++) {
-                    std::cout << " " << matrix[row*cols + col];
-                }
-                std::cout <<  " ]\n";
-            }
-        }
+    enum init_type_t { RANDOM, FORGY, PLUSPLUS } g_init; // May have to use
+    enum kms_stage_t { INIT, ESTEP } g_stage; // What phase of the algo we're in
 
-    class cluster {
+    typedef std::pair<edge_seq_iterator, data_seq_iterator> seq_iter;
+
+    class cluster
+    {
         private:
             std::vector<double> mean; // cluster mean
             unsigned num_members; // cluster assignment count
@@ -84,6 +72,7 @@ namespace {
                 for (unsigned i = 0; i < mean.size(); i++) {
                     mean[i] /= val;
                 }
+                complete = true;
             }
 
             cluster(const unsigned len) {
@@ -121,14 +110,161 @@ namespace {
                 return num_members;
             }
 
+            const bool is_complete() const {
+                return complete;
+            }
+
+            const unsigned size() const {
+                return mean.size();
+            }
+
+            // TODO: Make args const
+            void add_member(edge_seq_iterator& id_it, data_seq_iterator& count_it) {
+                while(id_it.has_next()) {
+                    vertex_id_t nid = id_it.next();
+                    edge_count e = count_it.next();
+                    mean[nid] += e.get_count();
+                }
+                num_members++;
+            }
+
             cluster& operator=(const cluster& other) {
                 this->mean = other.get_mean();
                 this->num_members = other.get_num_members();
                 return *this;
             }
-    };
-}
 
+            double& operator[] (const unsigned index) {
+                assert(index >= mean.size());
+                return mean[index];
+            }
+
+            cluster& operator+=(cluster& rhs) {
+                assert(rhs.size() == size());
+                // TODO vectorize perhaps
+                for (unsigned i = 0; i < mean.size(); i++) {
+                    this->mean[i] += rhs[i];
+                }
+                this->num_members += rhs.get_num_members();
+                return *this;
+            }
+    };
+
+    // Helper
+    static void print_clusters(std::vector<cluster::ptr>& clusters) {
+        for (std::vector<cluster::ptr>::iterator it = clusters.begin(); 
+                it != clusters.end(); ++it) {
+            print_vector<double>((*it)->get_mean());
+        }
+    }
+
+    class kmeans_vertex: public compute_vertex
+    {
+        unsigned cluster_id;
+
+        public:
+        kmeans_vertex(vertex_id_t id):
+            compute_vertex(id) { }
+
+        unsigned get_result() const {
+            return cluster_id;
+        }
+
+        const vsize_t get_cluster_id() const {
+            return cluster_id;
+        }
+
+        void run(vertex_program &prog) {
+            vertex_id_t id = prog.get_vertex_id(*this);
+            request_vertices(&id, 1);
+        }
+
+        void run(vertex_program& prog, const page_vertex &vertex) { 
+            switch (g_stage) {
+                case INIT:
+                    printf("In init\n");
+                    run_init(prog, vertex, g_init);
+                    break;
+                case ESTEP:
+                    printf("estep\n"); // TODO
+                    break;
+                default:
+                    assert(0);
+            }
+        }
+        void run_on_message(vertex_program& prog, const vertex_message& msg) { }
+
+        void run_init(vertex_program& prog, const page_vertex &vertex, init_type_t init);
+    };
+
+    /* Used in per thread cluster formation */
+    class kmeans_vertex_program : public vertex_program_impl<kmeans_vertex>
+    {
+        std::vector<cluster::ptr> pt_clusters;
+
+        public:
+        typedef std::shared_ptr<kmeans_vertex_program> ptr;
+
+        kmeans_vertex_program() {
+            for (unsigned thd = 0; thd < K; thd++) {
+                pt_clusters.push_back(cluster::create(NUM_COLS));
+            }
+        }
+
+        static ptr cast2(vertex_program::ptr prog) {
+            return std::static_pointer_cast<kmeans_vertex_program, vertex_program>(prog);
+        }
+
+        const std::vector<cluster::ptr>& get_pt_clusters() {
+            return pt_clusters;
+        }
+
+        cluster::ptr get_cluster(const unsigned id) {
+            return pt_clusters[id];
+        }
+    };
+
+    class kmeans_vertex_program_creater: public vertex_program_creater
+    {
+        public:
+            vertex_program::ptr create() const {
+                return vertex_program::ptr(new kmeans_vertex_program());
+            }
+    };
+
+    void kmeans_vertex::run_init(vertex_program& prog, const page_vertex &vertex, init_type_t init) {
+        switch (g_init) {
+            case RANDOM:
+                {
+                    this->cluster_id = random() % K;
+#if KM_TEST
+                    BOOST_LOG_TRIVIAL(info) << "Random init: v"<< prog.get_vertex_id(*this)
+                        << " assigned to cluster: c" <<  cluster_id;
+#endif
+                    kmeans_vertex_program& vprog = (kmeans_vertex_program&) prog;
+
+                    edge_seq_iterator id_it = vertex.get_neigh_seq_it(OUT_EDGE); // TODO: Make sure OUT_EDGE has data
+                    data_seq_iterator count_it = ((const page_directed_vertex&)vertex).
+                        get_data_seq_it<edge_count>(OUT_EDGE); //TODO: Make sure we have a directed graph
+                    vprog.get_cluster(cluster_id)->add_member(id_it, count_it);
+                }
+                break;
+            case FORGY:
+                printf("Forgy init\n"); // TODO
+                assert(0);
+                break;
+            case PLUSPLUS:
+                printf("PlusPlus init\n"); // TODO
+                assert(0);
+                break;
+            default:
+                assert(0);
+        }
+    }
+
+
+
+}
 
 namespace fg
 {
@@ -136,13 +272,17 @@ namespace fg
             const unsigned MAX_ITERS, const double tolerance)
     {
 #ifdef PROFILER
-        ProfilerStart("/home/disa/FlashGraph/flash-graph/matrix/kmeans.perf");
+        ProfilerStart("/home/disa/FlashGraph/flash-graph/libgraph-algs/sem_kmeans.perf");
 #endif
+        graph_index::ptr index = NUMA_graph_index<kmeans_vertex>::create(
+                fg->get_graph_header());
+        graph_engine::ptr mat = fg->create_engine(index);
+
         K = k;
         std::vector<cluster::ptr> clusters; // cluster means/centers
         std::vector<unsigned> cluster_assignments; // Which cluster a sample is in
-        NUM_ROWS = 0; // FIXME: Get from graph or header
-        NUM_COLS = 0; // FIXME: Store the maximum dimension of the matrix in header
+        NUM_ROWS = mat->get_max_vertex_id();
+        NUM_COLS = 5; // FIXME: Store the maximum dimension of the matrix in header
 
         // Check k
         if (K > NUM_ROWS || K < 2 || K == (unsigned)-1) {
@@ -168,24 +308,52 @@ namespace fg
             clusters.push_back(cluster::create(NUM_COLS));
 
         /*** End VarInit ***/
+        g_stage = INIT;
 
         if (init == "random") {
             BOOST_LOG_TRIVIAL(info) << "Init is '"<< init <<"'";
-            // random_partition_init(cluster_assignments); // TODO
-            // We must now update cluster centers before we begin
-            //M_step();
+            g_init = RANDOM;
         }
         if (init == "forgy") {
             BOOST_LOG_TRIVIAL(info) << "Init is '"<< init <<"'";
-            //forgy_init(); // TODO
-        }
-        else {
-            if (init == "kmeanspp") {
+            g_init = FORGY;
+        } else if (init == "kmeanspp") {
             BOOST_LOG_TRIVIAL(info) << "Init is '"<< init <<"'";
-                //kmeanspp_init(matrix, clusters); // TODO
-            }
+            g_init = PLUSPLUS;
         }
 
+        BOOST_LOG_TRIVIAL(info) << "Running init ...";
+
+        mat->start_all(vertex_initializer::ptr(),
+                vertex_program_creater::ptr(new kmeans_vertex_program_creater()));
+        mat->wait4complete();
+
+        // **** Functionize **** // 
+        BOOST_LOG_TRIVIAL(info) << "Verifying initial clusters";
+        FG_vector<unsigned>::ptr vec = FG_vector<unsigned>::create(mat);
+        mat->query_on_all(vertex_query::ptr(new save_query<unsigned, kmeans_vertex>(vec)));
+
+        std::vector<vertex_program::ptr> kms_clust_progs;
+        mat->get_vertex_programs(kms_clust_progs);
+
+        BOOST_FOREACH(vertex_program::ptr vprog, kms_clust_progs) {
+            kmeans_vertex_program::ptr kms_prog = kmeans_vertex_program::cast2(vprog);
+            std::vector<cluster::ptr> pt_clusters = kms_prog->get_pt_clusters();
+            /* Merge the per-thread clusters */
+            for(unsigned cl = 0; cl < K; cl++) {
+                *(clusters[cl]) += *(pt_clusters[cl]);
+            }
+            // TODO: Divide
+        }
+        // **** End Functionize **** // 
+
+        BOOST_LOG_TRIVIAL(info) << "Printing cluster assignments:";
+        vec->print();
+        BOOST_LOG_TRIVIAL(info) << "Printing cluster means:";
+        print_clusters(clusters);
+        exit(-1);
+
+        g_stage = ESTEP;
         BOOST_LOG_TRIVIAL(info) << "SEM-K||means starting ...";
 
         bool converged = false;
