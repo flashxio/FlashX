e7e9e0492b5addddf5239d4107996b8f4073446a
Disa Mhembere <dmhembe1@jhu.edu>
2015-10-07 03:12:29 -0400

    [graph]: removed would-be redeclared classes from sem_kmeans.cpp

--- flash-graph/libgraph-algs/sem_kmeans.cpp
+++ flash-graph/libgraph-algs/sem_kmeans.cpp
@@ -17,20 +17,11 @@
  * limitations under the License.
  */
 
+#include "sem_kmeans.h"
 #include <signal.h>
 #ifdef PROFILER
 #include <gperftools/profiler.h>
 #endif
-#include <math.h>
-
-#include <vector>
-#include <algorithm>
-#include <map>
-
-#include "graph_engine.h"
-#include "graph_config.h"
-#include "FGlib.h"
-#include "save_result.h"
 
 #define PRUNE 1
 #define KM_TEST 0
@@ -40,7 +31,6 @@
 using namespace fg;
 
 namespace {
-    typedef safs::page_byte_array::seq_const_iterator<edge_count> data_seq_iterator;
     typedef std::pair<double, double> distpair;
     static unsigned NUM_COLS;
     static unsigned NUM_ROWS;
@@ -73,116 +63,6 @@ namespace {
     enum init_type_t { RANDOM, FORGY, PLUSPLUS } g_init; // May have to use
     enum kmspp_stage_t { ADDMEAN, DIST } g_kmspp_stage; // Either adding a mean / computing dist
     enum kms_stage_t { INIT, ESTEP } g_stage; // What phase of the algo we're in
-
-    class cluster
-    {
-        private:
-            std::vector<double> mean; // cluster mean
-            unsigned num_members; // cluster assignment count
-            bool complete; // Have we already divided by num_members
-
-            void div(const unsigned val) {
-                if (num_members > 0) {
-                    for (unsigned i = 0; i < mean.size(); i++) {
-                        mean[i] /= double(val);
-                    }
-                }
-                complete = true;
-            }
-
-            cluster(const unsigned len) {
-                mean.assign(len, 0);
-                num_members = 0;
-                complete = false;
-            }
-
-            cluster(const std::vector<double>& mean) {
-                set_mean(mean);
-                num_members = 1;
-                complete = true;
-            }
-
-        public:
-            typedef typename std::shared_ptr<cluster> ptr;
-
-            static ptr create(const unsigned len) {
-                return ptr(new cluster(len));
-            }
-
-            static ptr create(const std::vector<double>& mean) {
-                return ptr(new cluster(mean));
-            }
-
-            void init(const unsigned len) {
-                mean.assign(len, 0);
-                num_members = 0;
-            }
-
-            void assign(const double val) {
-                mean.assign(mean.size(), val);
-            }
-
-            void clear() {
-                this->assign(0);
-                this->num_members = 0;
-                complete = false;
-            }
-
-            const std::vector<double>& get_mean() const {
-                return mean;
-            }
-
-            void set_mean(const std::vector<double>& mean) {
-                this->mean = mean;
-            }
-
-            const unsigned get_num_members() const {
-                return num_members;
-            }
-
-            const bool is_complete() const {
-                return complete;
-            }
-
-            const unsigned size() const {
-                return mean.size();
-            }
-
-            void finalize() {
-                assert(!complete);
-                this->div(this->num_members);
-            }
-
-            void add_member(edge_seq_iterator& id_it, data_seq_iterator& count_it) {
-                while(id_it.has_next()) {
-                    vertex_id_t nid = id_it.next();
-                    edge_count e = count_it.next();
-                    mean[nid] += e.get_count();
-                }
-                num_members++;
-            }
-
-            cluster& operator=(const cluster& other) {
-                this->mean = other.get_mean();
-                this->num_members = other.get_num_members();
-                return *this;
-            }
-
-            double& operator[](const unsigned index) {
-                assert(index < mean.size());
-                return mean[index];
-            }
-
-            cluster& operator+=(cluster& rhs) {
-                assert(rhs.size() == size());
-                // TODO vectorize perhaps
-                for (unsigned i = 0; i < mean.size(); i++) {
-                    this->mean[i] += rhs[i];
-                }
-                this->num_members += rhs.get_num_members();
-                return *this;
-            }
-    };
 
     static std::vector<cluster::ptr> g_clusters; // cluster means/centers
 
@@ -583,32 +463,6 @@ namespace {
 
 namespace fg
 {
-    // A class used a return object for R bindings
-    class sem_kmeans_ret
-    {
-        private:
-            FG_vector<unsigned>::ptr cluster_assignments;
-            std::vector<std::vector<double>> centers;
-            std::vector<unsigned> size;
-            unsigned iters;
-
-            sem_kmeans_ret(FG_vector<unsigned>::ptr cluster_assignments,
-                    std::vector<std::vector<double>> centers, std::vector<unsigned>& size, unsigned iters) {
-                this->cluster_assignments = cluster_assignments;
-                this->centers = centers;
-                this->size = size;
-                this->iters = iters;
-            }
-
-        public:
-            typedef typename std::shared_ptr<sem_kmeans_ret> ptr;
-
-            static ptr create(FG_vector<unsigned>::ptr cluster_assignments,
-                    std::vector<std::vector<double>> centers, std::vector<unsigned>& size, unsigned iters) {
-                return ptr(new sem_kmeans_ret(cluster_assignments, centers, size, iters));
-            }
-    };
-
     sem_kmeans_ret::ptr compute_sem_kmeans(FG_graph::ptr fg, const size_t k, const std::string init,
             const unsigned max_iters, const double tolerance, const double comp_thresh) {
 #ifdef PROFILER
