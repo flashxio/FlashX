fa08cf1974289ba306e6be1592694e4eb3fed033
Da Zheng <zhengda1936@gmail.com>
2015-11-25 20:17:10 -0500

    [R]: add which.min and which.max.

--- Rpkg/src/matrix_ops.cpp
+++ Rpkg/src/matrix_ops.cpp
@@ -364,6 +364,96 @@ public:
 	}
 };
 
+template<class T>
+class r_which_max_operate: public bulk_operate
+{
+public:
+	virtual void runAA(size_t num_eles, const void *left_arr,
+			const void *right_arr, void *output_arr) const {
+		throw unsupported_exception();
+	}
+	virtual void runAE(size_t num_eles, const void *left_arr,
+			const void *right, void *output_arr) const {
+		throw unsupported_exception();
+	}
+	virtual void runEA(size_t num_eles, const void *left,
+			const void *right_arr, void *output_arr) const {
+		throw unsupported_exception();
+	}
+
+	virtual void runAgg(size_t num_eles, const void *in, const void *orig,
+			void *output) const {
+		int *t_out = (int *) output;
+		const T *t_in = (const T *) in;
+		if (num_eles == 0)
+			return;
+		T max = t_in[0];
+		int idx = 0;
+		for (size_t i = 1; i < num_eles; i++) {
+			if (max < t_in[i]) {
+				max = t_in[i];
+				idx = i;
+			}
+		}
+		t_out[0] = idx + 1;
+	}
+
+	virtual const scalar_type &get_left_type() const {
+		return get_scalar_type<T>();
+	}
+	virtual const scalar_type &get_right_type() const {
+		return get_scalar_type<T>();
+	}
+	virtual const scalar_type &get_output_type() const {
+		return get_scalar_type<int>();
+	}
+};
+
+template<class T>
+class r_which_min_operate: public bulk_operate
+{
+public:
+	virtual void runAA(size_t num_eles, const void *left_arr,
+			const void *right_arr, void *output_arr) const {
+		throw unsupported_exception();
+	}
+	virtual void runAE(size_t num_eles, const void *left_arr,
+			const void *right, void *output_arr) const {
+		throw unsupported_exception();
+	}
+	virtual void runEA(size_t num_eles, const void *left,
+			const void *right_arr, void *output_arr) const {
+		throw unsupported_exception();
+	}
+
+	virtual void runAgg(size_t num_eles, const void *in, const void *orig,
+			void *output) const {
+		int *t_out = (int *) output;
+		const T *t_in = (const T *) in;
+		if (num_eles == 0)
+			return;
+		T min = t_in[0];
+		int idx = 0;
+		for (size_t i = 1; i < num_eles; i++) {
+			if (min > t_in[i]) {
+				min = t_in[i];
+				idx = i;
+			}
+		}
+		t_out[0] = idx + 1;
+	}
+
+	virtual const scalar_type &get_left_type() const {
+		return get_scalar_type<T>();
+	}
+	virtual const scalar_type &get_right_type() const {
+		return get_scalar_type<T>();
+	}
+	virtual const scalar_type &get_output_type() const {
+		return get_scalar_type<int>();
+	}
+};
+
 void init_udf_ext()
 {
 	std::vector<bulk_operate::const_ptr> ops;
@@ -372,6 +462,16 @@ void init_udf_ext()
 	ops.push_back(bulk_operate::const_ptr(new r_count_operate<int>()));
 	ops.push_back(bulk_operate::const_ptr(new r_count_operate<double>()));
 	register_udf(ops, "count");
+
+	ops.clear();
+	ops.push_back(bulk_operate::const_ptr(new r_which_max_operate<int>()));
+	ops.push_back(bulk_operate::const_ptr(new r_which_max_operate<double>()));
+	register_udf(ops, "which.max");
+
+	ops.clear();
+	ops.push_back(bulk_operate::const_ptr(new r_which_min_operate<int>()));
+	ops.push_back(bulk_operate::const_ptr(new r_which_min_operate<double>()));
+	register_udf(ops, "which.min");
 
 	std::vector<bulk_uoperate::const_ptr> uops;
 	uops.push_back(bulk_uoperate::conv2ptr(
