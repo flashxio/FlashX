be4401a32c298dfc75cab073d4a8193334a29f99
Da Zheng <zhengda1936@gmail.com>
2015-12-05 20:18:17 -0500

    [Doc]: rename FlashGraphR to FlashR.

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo 'roxygen2::roxygenize("Rpkg/")' | R --no-save
-mkdir -p docs/FlashGraphR
+mkdir -p docs/FlashR
 for file in `ls Rpkg/man/`
 do
 	input_file="Rpkg/man/$file"
-	output_file="docs/FlashGraphR/${file}.html"
+	output_file="docs/FlashR/${file}.html"
 	echo "tools::Rd2HTML(\"$input_file\", out=\"$output_file\")" | R --no-save
 done
