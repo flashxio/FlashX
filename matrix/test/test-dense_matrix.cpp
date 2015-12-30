1d9422faf336a968da245960ef43f71a8edb6f6f
Da Zheng <zhengda1936@gmail.com>
2015-12-30 10:10:02 -0500

    [Matrix]: add mapply to perf test.

--- matrix/test/test-dense_matrix.cpp
+++ matrix/test/test-dense_matrix.cpp
@@ -4,6 +4,8 @@
 
 using namespace fm;
 
+size_t length = 4096L * 1024 * 1024;
+
 class mat_init: public type_set_operate<int>
 {
 public:
@@ -13,6 +15,56 @@ public:
 			arr[i] = i;
 	}
 };
+
+class mat_init1: public type_set_operate<float>
+{
+	size_t num_vertices;
+public:
+	mat_init1(size_t num_vertices) {
+		this->num_vertices = num_vertices;
+	}
+
+	virtual void set(float *arr, size_t num_eles, off_t row_idx,
+			            off_t col_idx) const {
+		for (size_t i = 0; i < num_eles; i++)
+			arr[i] = 1.0 / num_vertices;
+	}
+};
+
+void test_mapply()
+{
+	struct timeval start, end;
+	int num_nodes = matrix_conf.get_num_nodes();
+	printf("mapply on %d nodes\n", num_nodes);
+	gettimeofday(&start, NULL);
+	detail::matrix_store::ptr tmp = detail::matrix_store::create(
+			length, 1, matrix_layout_t::L_ROW,
+			get_scalar_type<float>(), num_nodes, true);
+	detail::matrix_store::ptr tmp1 = detail::matrix_store::create(
+			length, 1, matrix_layout_t::L_ROW,
+			get_scalar_type<float>(), num_nodes, true);
+	tmp->set_data(mat_init());
+	printf("init vec1 of %ld eles\n", tmp->get_num_rows());
+	tmp1->set_data(mat_init1(tmp1->get_num_rows()));
+	printf("init vec2 of %ld eles\n", tmp1->get_num_rows());
+	gettimeofday(&end, NULL);
+	printf("init takes %.3f seconds\n", time_diff(start, end));
+
+	start = end;
+	dense_matrix::ptr pr = dense_matrix::create(tmp);
+	dense_matrix::ptr pr1 = dense_matrix::create(tmp1);
+	pr = pr->multiply_scalar<float>(0.85);
+	pr = pr->add_scalar<float>(0.15 / length);
+	dense_matrix::ptr diff = pr1->minus(*pr);
+	diff = diff->abs();
+	gettimeofday(&end, NULL);
+	printf("virtual computation takes %.3f seconds\n", time_diff(start, end));
+	dense_matrix::ptr comp = diff->lt_scalar<float>(0.00001);
+	scalar_variable::ptr convg = comp->sum();
+	assert(convg->get_type() == get_scalar_type<size_t>());
+	gettimeofday(&end, NULL);
+	printf("test takes %.3f seconds\n", time_diff(start, end));
+}
 
 dense_matrix::ptr create_mat(size_t ncol, matrix_layout_t layout)
 {
@@ -99,6 +151,8 @@ int main(int argc, char *argv[])
 		test_sapply();
 	else if (test_name == "apply_scalar")
 		test_apply_scalar();
+	else if (test_name == "mapply")
+		test_mapply();
 	else {
 		fprintf(stderr, "unknow test\n");
 		return -1;
