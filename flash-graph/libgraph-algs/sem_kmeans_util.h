7f3cd1ec925d6b492c9276277e14d59db82a1f89
Disa Mhembere <dmhembe1@jhu.edu>
2015-12-31 03:13:25 -0500

    [Graph]: Moved common classes to sem_kmeans_util

--- flash-graph/libgraph-algs/sem_kmeans_util.h
+++ flash-graph/libgraph-algs/sem_kmeans_util.h
@@ -30,50 +30,147 @@ using namespace fg;
 
 namespace {
 
+    template <typename T>
+        static void print_vector(typename std::vector<T> v, unsigned max_print=100) {
+            unsigned print_len = v.size() > max_print ? max_print : v.size();
+
+            std::cout << "[";
+            typename std::vector<T>::iterator itr = v.begin();
+            for (; itr != v.begin()+print_len; itr++) {
+                std::cout << " "<< *itr;
+            }
+
+            if (v.size() > print_len) std::cout << " ...";
+            std::cout <<  " ]\n";
+        }
+
     // A very C-style binary data reader
     template <typename T>
         class bin_reader {
             private:
-            FILE* f;
-            unsigned nrow, ncol;
+                FILE* f;
+                unsigned nrow, ncol;
 
-            void cat(const T* arr) {
-                std::cout << "[ ";
-                for (unsigned i = 0; i < ncol; i++) {
-                    std::cout << arr[i] << " ";
+                void cat(const T* arr) {
+                    std::cout << "[ ";
+                    for (unsigned i = 0; i < ncol; i++) {
+                        std::cout << arr[i] << " ";
+                    }
+                    std::cout << "]\n";
                 }
-                std::cout << "]\n";
-            }
 
             public:
-            bin_reader(std::string fn, unsigned nrow, unsigned ncol) {
-                f = fopen(fn.c_str(), "rb");
-                BOOST_VERIFY(NULL != f);
-                this->nrow = nrow;
-                this->ncol = ncol;
-            }
-
-            // Read data and cat in a viewer friendly fashion
-            void read_cat() {
-                T arr [ncol];
-                for (unsigned i = 0; i < nrow; i++) {
-                    size_t num_read = fread(&arr[0], sizeof(T)*ncol, 1, f);
-                    BOOST_ASSERT_MSG(num_read == 1, "Error reading file!\n");
-                    cat(arr);
+                bin_reader(std::string fn, unsigned nrow, unsigned ncol) {
+                    f = fopen(fn.c_str(), "rb");
+                    BOOST_VERIFY(NULL != f);
+                    this->nrow = nrow;
+                    this->ncol = ncol;
                 }
-            }
 
-            std::vector<double> readline() {
-                std::vector<double> v;
-                v.resize(ncol);
-                size_t num_read = fread(&v[0], sizeof(v[0])*ncol, 1, f);
-                BOOST_ASSERT_MSG(num_read == 1, "Error reading file!\n");
-                return v;
-            }
+                // Read data and cat in a viewer friendly fashion
+                void read_cat() {
+                    T arr [ncol];
+                    for (unsigned i = 0; i < nrow; i++) {
+                        size_t num_read = fread(&arr[0], sizeof(T)*ncol, 1, f);
+                        BOOST_ASSERT_MSG(num_read == 1, "Error reading file!\n");
+                        cat(arr);
+                    }
+                }
 
-            ~bin_reader() {
-                fclose(f);
-            }
+                std::vector<double> readline() {
+                    std::vector<double> v;
+                    v.resize(ncol);
+                    size_t num_read = fread(&v[0], sizeof(v[0])*ncol, 1, f);
+                    BOOST_ASSERT_MSG(num_read == 1, "Error reading file!\n");
+                    return v;
+                }
+
+                ~bin_reader() {
+                    fclose(f);
+                }
         };
+
+    // Class to hold stats on the effectiveness of pruning
+    class prune_stats
+    {
+        private:
+            // Counts per iteration
+            std::atomic<unsigned> lemma1, _3a, _3b, _3c, _4;
+
+            // Total counts
+            unsigned tot_lemma1, tot_3a, tot_3b, tot_3c, tot_4, iter;
+            unsigned NUM_ROWS;
+            unsigned K;
+
+            prune_stats(const unsigned nrows, const unsigned nclust) {
+                _3a = _3b = lemma1 = _3c = _4 = 0;
+                tot_lemma1 = tot_3a = tot_3b = tot_3c = tot_4 = iter = 0;
+
+                this->NUM_ROWS = nrows;
+                this->K = nclust;
+            }
+
+        public:
+            typedef std::shared_ptr<prune_stats> ptr;
+
+            static ptr create(const unsigned nrows, const unsigned nclust) {
+                return ptr(new prune_stats(nrows, nclust));
+            }
+            void pp_lemma1(unsigned var=1) {
+                lemma1 = lemma1 + var;
+            }
+            void pp_3a() {
+                _3a = _3a + 1;
+            }
+            void pp_3b() {
+                _3b = _3b + 1;
+            }
+            void pp_3c() {
+                _3c = _3c + 1;
+            }
+            void pp_4() {
+                _4 = _4 + 1;
+            }
+            void finalize() {
+                iter++;
+                BOOST_VERIFY((lemma1 + _3a + _3b + _3c + _4) <=  NUM_ROWS*K);
+                BOOST_LOG_TRIVIAL(info) << "\n\nPrune stats count:\n"
+                    "lemma1 = " << lemma1 << ", 3a = " << _3a
+                    << ", 3b = " << _3b << ", 3c = " << _3c << ", 4 = " << _4;
+
+                BOOST_LOG_TRIVIAL(info) << "\n\nPrune stats \%s:\n"
+                    "lemma1 = " << (lemma1 == 0 ? 0 : ((double)lemma1/(NUM_ROWS*K))*100) <<
+                    "\%, 3a = " << (_3a == 0 ? 0 : ((double)_3a/(NUM_ROWS*K))*100) <<
+                    "\%, 3b = " << (_3b == 0 ? 0 : ((double) _3b/(NUM_ROWS*K))*100) <<
+                    "\%, 3c = " << (_3c == 0 ? 0 : ((double) _3c/(NUM_ROWS*K))*100) <<
+                    "\%, 4 = " << (_4 == 0 ? 0 : ((double) _4/(NUM_ROWS*K))*100) << "\%";
+
+                tot_lemma1 += (unsigned)lemma1;
+                tot_3a += (unsigned)_3a;
+                tot_3b += (unsigned)_3b;
+                tot_3c += (unsigned)_3c;
+                tot_4 += (unsigned)_4;
+
+                lemma1 = _3a = _3b = _3c = _4 = 0; // reset
+            }
+
+            std::vector<double> get_stats() {
+                double perc_lemma1 = tot_lemma1 / ((double)(NUM_ROWS*this->iter*K))*100;
+                double perc_3a = tot_3a / ((double)(NUM_ROWS*this->iter*K))*100;
+                double perc_3b = tot_3b / ((double)(NUM_ROWS*this->iter*K))*100;
+                double perc_3c = tot_3c / ((double)(NUM_ROWS*this->iter*K))*100;
+                double perc_4 = tot_4 / ((double)(NUM_ROWS*this->iter*K))*100;
+                // Total percentage
+                double perc = ((tot_3b + tot_3a + tot_3c + tot_4 + tot_lemma1) /
+                        ((double)((NUM_ROWS*this->iter*K)) + ((K*(K-1))/2.0)))*100;
+                BOOST_LOG_TRIVIAL(info) << "\n\nPrune stats total:\n"
+                    "Tot = " << perc << "\%, 3a = " << perc_3a <<
+                    "\%, 3b = " << perc_3b << "\%, 3c = " << perc_3c
+                    << "\%, 4 = " << perc_4 << "\%, lemma1 = " << perc_lemma1 << "\%";
+
+                std::vector<double> ret {perc_lemma1, perc_3a, perc_3b, perc_3c, perc_4, perc};
+                return ret;
+            }
+    };
 }
 #endif
