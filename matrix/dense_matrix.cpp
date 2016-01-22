4d25409028f6e16ed5efd0bbe898dd7bd8686cf2
Da Zheng <zhengda1936@gmail.com>
2016-01-21 20:47:37 -0500

    [Matrix]: slightly change conv2 to avoid loading the col to mem.

--- matrix/dense_matrix.cpp
+++ matrix/dense_matrix.cpp
@@ -2918,16 +2918,18 @@ dense_matrix::ptr dense_matrix::conv2(matrix_layout_t layout) const
 
 	// If the dense matrix has only one row or one column, it's very easy
 	// to convert its layout. We don't need to copy data or run computation
-	// at all. This only works if the matrix isn't virtual. If this is
-	// a virtual matrix, get a row/col from the matrix will trigger
-	// materialization.
-	// TODO It's a problem that an EM matrix may become in-memory.
-	if (get_num_cols() == 1 && !get_data().is_virtual()) {
+	// at all. This only works for in-mem non-virtual matrices. If this is
+	// a virtual matrix, getting a row/col from the matrix will trigger
+	// materialization. If this is an EM matrix, it'll result in reading
+	// the entire column to memory.
+	if (get_num_cols() == 1 && !get_data().is_virtual()
+			&& get_data().is_in_mem()) {
 		detail::vec_store::const_ptr vec = get_data().get_col_vec(0);
 		return dense_matrix::create(vec->conv2mat(get_num_rows(),
 					get_num_cols(), layout == matrix_layout_t::L_ROW));
 	}
-	else if (get_num_rows() == 1 && !get_data().is_virtual()) {
+	else if (get_num_rows() == 1 && !get_data().is_virtual()
+			&& get_data().is_in_mem()) {
 		detail::vec_store::const_ptr vec = get_data().get_row_vec(0);
 		return dense_matrix::create(vec->conv2mat(get_num_rows(),
 					get_num_cols(), layout == matrix_layout_t::L_ROW));
