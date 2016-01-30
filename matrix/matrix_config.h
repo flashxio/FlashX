5c60540c4bbdd4c6e4636728f403ddb7ed5cbab1
Da Zheng <zhengda1936@gmail.com>
2016-01-30 16:01:36 -0500

    [Matrix]: detect #threads and #nodes with HWLOC.

--- matrix/matrix_config.h
+++ matrix/matrix_config.h
@@ -1,7 +1,7 @@
 #ifndef __MATRIX_CONFIG_H__
 #define __MATRIX_CONFIG_H__
 
-/**
+/*
  * Copyright 2014 Open Connectome Project (http://openconnecto.me)
  * Written by Da Zheng (zhengda1936@gmail.com)
  *
@@ -237,105 +237,6 @@ public:
 		return keep_mem_buf;
 	}
 };
-
-inline void matrix_config::print_help()
-{
-	printf("Configuration parameters in matrix operations.\n");
-	printf("\tthreads: the number of threads processing the matrix\n");
-	printf("\tFM_prof_file: the output file containing CPU profiling\n");
-	printf("\tin_mem_matrix: indicate whether to load the entire matrix to memory in advance\n");
-	printf("\trow_block_size: the size of a row block (the number of rows)\n");
-	printf("\trb_io_size: the size of a matrix I/O in 1D (the number of row blocks)\n");
-	printf("\trb_steal_io_size: the size of a stolen matrix I/O(the number of row blocks)\n");
-	printf("\tcpu_cache_size: the cpu cache size that can be used by a thread\n");
-	printf("\thilbert_order: use the hilbert order\n");
-	printf("\tnum_nodes: The number of NUMA nodes\n");
-	printf("\tsort_buf_size: the buffer size for EM sorting\n");
-	printf("\tgroupby_buf_size: the buffer size for EM groupby on vectors\n");
-	printf("\tvv_groupby_buf_size: the buffer size for EM groupby on vector vectors\n");
-	printf("\twrite_io_buf_size: the I/O buffer size for writing merge results\n");
-	printf("\tstream_io_size: the I/O size used for streaming\n");
-	printf("\tkeep_mem_buf: indicate whether to keep memory buffer for I/O in dense matrix operation\n");
-}
-
-inline void matrix_config::print()
-{
-	BOOST_LOG_TRIVIAL(info) << "Configuration parameters in matrix operations.";
-	BOOST_LOG_TRIVIAL(info) << "\tSpM threads: " << num_SpM_threads;
-	BOOST_LOG_TRIVIAL(info) << "\tDM threads: " << num_DM_threads;
-	BOOST_LOG_TRIVIAL(info) << "\tFM_prof_file: " << prof_file;
-	BOOST_LOG_TRIVIAL(info) << "\tin_mem_matrix: " << _in_mem_matrix;
-	BOOST_LOG_TRIVIAL(info) << "\trow_block_size: " << row_block_size;
-	BOOST_LOG_TRIVIAL(info) << "\trb_io_size: " << rb_io_size;
-	BOOST_LOG_TRIVIAL(info) << "\trb_steal_io_size: " << rb_steal_io_size;
-	BOOST_LOG_TRIVIAL(info) << "\tcpu_cache_size: " << cpu_cache_size;
-	BOOST_LOG_TRIVIAL(info) << "\thilbert_order: " << hilbert_order;
-	BOOST_LOG_TRIVIAL(info) << "\tnum_nodes: " << num_nodes;
-	BOOST_LOG_TRIVIAL(info) << "\tsort_buf_size: " << sort_buf_size;
-	BOOST_LOG_TRIVIAL(info) << "\tgroupby_buf_size: " << groupby_buf_size;
-	BOOST_LOG_TRIVIAL(info) << "\tvv_groupby_buf_size: " << vv_groupby_buf_size;
-	BOOST_LOG_TRIVIAL(info) << "\twrite_io_buf_size: " << write_io_buf_size;
-	BOOST_LOG_TRIVIAL(info) << "\tstream_io_size: " << stream_io_size;
-	BOOST_LOG_TRIVIAL(info) << "\tkeep_mem_buf: " << keep_mem_buf;
-}
-
-inline void matrix_config::init(config_map::ptr map)
-{
-	// `threads' sets #threads for sparse matrix and dense matrix operations.
-	// But users can customize #threads for sparse matrix and dense matrix
-	// separately.
-	if (map->has_option("threads")) {
-		map->read_option_int("threads", num_SpM_threads);
-		map->read_option_int("threads", num_DM_threads);
-	}
-	if (map->has_option("SpM_threads"))
-		map->read_option_int("SpM_threads", num_SpM_threads);
-	if (map->has_option("DM_threads"))
-		map->read_option_int("DM_threads", num_DM_threads);
-	if (map->has_option("FM_prof_file"))
-		map->read_option("FM_prof_file", prof_file);
-	if (map->has_option("in_mem_matrix"))
-		map->read_option_bool("in_mem_matrix", _in_mem_matrix);
-	if (map->has_option("row_block_size"))
-		map->read_option_int("row_block_size", row_block_size);
-	if (map->has_option("rb_io_size"))
-		map->read_option_int("rb_io_size", rb_io_size);
-	if (map->has_option("rb_steal_io_size"))
-		map->read_option_int("rb_steal_io_size", rb_steal_io_size);
-	if (map->has_option("cpu_cache_size"))
-		map->read_option_int("cpu_cache_size", cpu_cache_size);
-	if (map->has_option("hilbert_order"))
-		map->read_option_bool("hilbert_order", hilbert_order);
-	if (map->has_option("num_nodes"))
-		map->read_option_int("num_nodes", num_nodes);
-	if (map->has_option("sort_buf_size")) {
-		long tmp = 0;
-		map->read_option_long("sort_buf_size", tmp);
-		sort_buf_size = tmp;
-	}
-	if (map->has_option("groupby_buf_size")) {
-		long tmp = 0;
-		map->read_option_long("groupby_buf_size", tmp);
-		groupby_buf_size = tmp;
-	}
-	if (map->has_option("vv_groupby_buf_size")) {
-		long tmp = 0;
-		map->read_option_long("vv_groupby_buf_size", tmp);
-		vv_groupby_buf_size = tmp;
-	}
-	if (map->has_option("write_io_buf_size")) {
-		long tmp = 0;
-		map->read_option_long("write_io_buf_size", tmp);
-		write_io_buf_size = tmp;
-	}
-	if (map->has_option("stream_io_size")) {
-		long tmp = 0;
-		map->read_option_long("stream_io_size", tmp);
-		stream_io_size = tmp;
-	}
-	if (map->has_option("keep_mem_buf"))
-		map->read_option_bool("keep_mem_buf", keep_mem_buf);
-}
 
 extern matrix_config matrix_conf;
 
