060dcbe77d74ef8e0007cbbaf37ac1edc3584877
Da Zheng <zhengda1936@gmail.com>
2016-01-31 13:27:45 -0500

    Add index.html for R docs.

---
+++ docs/FlashR/generate_links.py
@@ -0,0 +1,10 @@
+import glob
+
+DIRECTORY = "."
+ROOT_URL = "http://flashx.io/docs/FlashR/"
+
+links = []
+for f in glob.glob('{}/*.html'.format(DIRECTORY)):
+    links.append("<li><a href='{}{}'>{}</a></li>".format(ROOT_URL, f, f))
+
+print("<ul>" + "\n".join(links) + "</ul>")
