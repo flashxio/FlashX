44b8947b0e7ae257ce313a57a071dacb6b3e2787
Da Zheng <zhengda1936@gmail.com>
2016-01-31 13:30:37 -0500

    get the file name in the path.

--- docs/FlashR/generate_links.py
+++ docs/FlashR/generate_links.py
@@ -1,10 +1,12 @@
 import glob
+import ntpath
 
 DIRECTORY = "."
 ROOT_URL = "http://flashx.io/docs/FlashR/"
 
 links = []
 for f in glob.glob('{}/*.html'.format(DIRECTORY)):
+    f = ntpath.basename(f)
     links.append("<li><a href='{}{}'>{}</a></li>".format(ROOT_URL, f, f))
 
 print("<ul>" + "\n".join(links) + "</ul>")
