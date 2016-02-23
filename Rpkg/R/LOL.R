96bc8cb55015259fdfd0219f642a7c4405945c59
Da Zheng <zhengda1936@gmail.com>
2016-02-23 14:12:29 -0500

    [R]: add LOL.

---
+++ Rpkg/R/LOL.R
@@ -0,0 +1,14 @@
+# m is a D x I matrix. Each column is a training instance and each instance has D features.
+# labels contains I labels, one for each training instance
+# k specifies #columns of the output matrix
+# The output is a D x k matrix.
+LOL <- function(m, labels, k) {
+	counts <- fm.table(labels)
+	num.labels <- length(counts$val)
+	num.features <- dim(m)[1]
+	nv <- k - num.labels
+	gr.sum <- fm.groupby(m, 1, fm.as.factor(labels, 2), fm.bo.add)
+	gr.mean <- fm.mapply.row(gr.sum, counts$Freq, fm.bo.div, FALSE)
+	svd <- fm.svd(m, nv=0, nu=nv)
+	fm.cbind(gr.mean, svd$u)
+}
