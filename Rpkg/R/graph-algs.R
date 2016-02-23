9b42fd4004d235c940d94ab5658f761fe0306dc6
Da Zheng <zhengda1936@gmail.com>
2016-02-22 20:32:17 -0500

    [R]: explicitly trigger GC in PageRank.

--- Rpkg/R/graph-algs.R
+++ Rpkg/R/graph-algs.R
@@ -16,6 +16,8 @@ PageRank <- function(A, d = 0.15, max.niters = 30, epsilon = 1e-2,
 	A <- t(A)
 	while (L1 >= epsilon && niters < max.niters) {
 		pr2 <- d/N+(1-d)*(A %*% (pr1/out.deg))
+		gc()
+		pr2 <- fm.materialize(pr2)
 		diff <- abs(pr1-pr2)
 		L1 <- sum(diff)
 		if (verbose)
