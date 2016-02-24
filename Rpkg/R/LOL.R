bcf1c86c90d9f9d4da062bf470e8d808e2e7ad23
Da Zheng <zhengda1936@gmail.com>
2016-02-23 19:14:58 -0500

    [R]: compute diff of means in LOL.

--- Rpkg/R/LOL.R
+++ Rpkg/R/LOL.R
@@ -6,9 +6,10 @@ LOL <- function(m, labels, k) {
 	counts <- fm.table(labels)
 	num.labels <- length(counts$val)
 	num.features <- dim(m)[1]
-	nv <- k - num.labels
+	nv <- k - (num.labels - 1)
 	gr.sum <- fm.groupby(m, 1, fm.as.factor(labels, 2), fm.bo.add)
 	gr.mean <- fm.mapply.row(gr.sum, counts$Freq, fm.bo.div, FALSE)
+	diff <- fm.get.cols(gr.mean, 1) - fm.get.cols(gr.mean, 2)
 	svd <- fm.svd(m, nv=0, nu=nv)
-	fm.cbind(gr.mean, svd$u)
+	fm.cbind(diff, svd$u)
 }
