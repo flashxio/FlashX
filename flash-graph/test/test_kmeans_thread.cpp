b376316e9109cec4b87d96ef3354b18288d47a77
Disa Mhembere <disa@jhu.edu>
2016-03-11 03:10:56 -0500

    [Test]: Added test for numa mem alloc

--- flash-graph/test/test_kmeans_thread.cpp
+++ flash-graph/test/test_kmeans_thread.cpp
@@ -18,32 +18,68 @@
  */
 
 #include <stdlib.h>
-#include <vector>
 #include "matrix/kmeans_thread.h"
 #include "matrix/kmeans_thread.cpp"
+#include "libgraph-algs/sem_kmeans_util.h"
 
 void test_thread_creation(const unsigned NTHREADS, const unsigned nnodes) {
-    std::vector<kmeans_thread> thds;
+    std::vector<kmeans_thread::ptr> thds;
 
     // Always: Build state alone
     for (unsigned i = 0; i < NTHREADS; i++)
-        thds.push_back(kmeans_thread(i%nnodes, i, 69, 200));
+        thds.push_back(kmeans_thread::create
+                (i%nnodes, i, 69, 200, 2, nullptr, NULL, "/dev/null"));
 
     // Always: Start threads alone
     for (unsigned i = 0; i < NTHREADS; i++)
-        thds[i].start();
+        thds[i]->start(TEST);
 
     unsigned sum = 0;
     unsigned verification = 0;
     for (unsigned i = 0; i < thds.size(); i++) {
-        thds[i].join();
-        sum += thds[i].get_val(); // OK to do together
+        thds[i]->join();
+        sum += thds[i]->get_val(); // OK to do together
         verification += i;
     }
     BOOST_VERIFY(sum == verification);
+    std::cout << "SUCCESS: The sum is: " << sum << "\n";
+}
 
-    std::cout << "In main the sum is: " << sum << "\n";
-    pthread_exit(NULL);
+void test_numa_populate_data() {
+    constexpr unsigned NTHREADS = 10;
+    constexpr unsigned nnodes = 4;
+    constexpr unsigned nrow = 50;
+    const unsigned nprocrows = nrow/NTHREADS;
+    constexpr unsigned ncol = 5;
+    const std::string fn = "/mnt/nfs/disa/data/tiny/matrix_r50_c5_rrw.bin";
+
+    std::vector<kmeans_thread::ptr> thds;
+
+    // Always: Build state alone
+    for (unsigned i = 0; i < NTHREADS; i++)
+        thds.push_back(kmeans_thread::create
+                (i%nnodes, i, i*nprocrows*ncol, nprocrows, ncol, nullptr, NULL, fn));
+
+    bin_reader<double> br(fn, nrow, ncol);
+    double* data = new double [nrow*ncol];
+    printf("Bin read data\n");
+    br.read(data);
+
+    // Allocate & move
+    std::vector<kmeans_thread::ptr>::iterator it = thds.begin();
+    for (; it != thds.end(); ++it)
+        (*it)->start(ALLOC_DATA);
+
+    for (it = thds.begin(); it != thds.end(); ++it)
+        (*it)->join();
+
+    // Print it back
+    for (it = thds.begin(); it != thds.end(); ++it) {
+        double *dp = &data[(*it)->get_thd_id()*ncol*nprocrows];
+        BOOST_VERIFY(eq_all(dp, (*it)->get_local_data(), nprocrows*ncol));
+        printf("Thread %u PASSED numa_mem_alloc()\n", (*it)->get_thd_id());
+    }
+    delete [] data;
 }
 
 int main(int argc, char* argv[]) {
@@ -54,5 +90,7 @@ int main(int argc, char* argv[]) {
     }
 
     test_thread_creation(atoi(argv[1]), atoi(argv[2]));
+    test_numa_populate_data();
+
     return (EXIT_SUCCESS);
 }
