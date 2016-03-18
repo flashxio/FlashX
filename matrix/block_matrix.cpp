25c1ba360b0ae02340d7b878d8679d714aeac4db
Da Zheng <zhengda1936@gmail.com>
2016-03-18 13:15:33 -0400

    [Matrix]: optimize matrix multiply on block matrix.
    It's better to give a larger matrix to BLAS for optimization instead of
    breaking it and giving them to BLAS separately.

--- matrix/block_matrix.cpp
+++ matrix/block_matrix.cpp
@@ -583,10 +583,94 @@ dense_matrix::ptr block_matrix::multiply_tall(const dense_matrix &m,
 	return block_matrix::inner_prod_tall(m, NULL, NULL, out_layout);
 }
 
+static void get_wider_matrices(detail::combined_matrix_store::const_ptr in,
+		std::vector<detail::matrix_store::const_ptr> &mats)
+{
+	size_t short_dim = std::min(in->get_mat_ref(0).get_num_rows(),
+			in->get_mat_ref(0).get_num_cols());
+	size_t num_blocks = 256 / short_dim;
+	if (num_blocks <= 1) {
+		for (size_t i = 0; i < in->get_num_mats(); i++)
+			mats[i] = in->get_mat(i);
+	}
+	else {
+		for (size_t i = 0; i < in->get_num_mats(); i += num_blocks) {
+			std::vector<detail::matrix_store::const_ptr> tmp(std::min(
+						in->get_num_mats() - i, num_blocks));
+			for (size_t j = 0; j < tmp.size(); j++)
+				tmp[j] = in->get_mat(i + j);
+			mats.push_back(detail::combined_matrix_store::create(tmp,
+						in->store_layout()));
+		}
+	}
+}
+
 dense_matrix::ptr block_matrix::multiply_wide(const dense_matrix &m,
 		matrix_layout_t out_layout) const
 {
-	return block_matrix::inner_prod_wide(m, NULL, NULL, out_layout);
+	if (is_in_mem()) {
+		dense_matrix::ptr ret = dense_matrix::multiply(m, out_layout);
+		ret->materialize_self();
+		return ret;
+	}
+
+	std::vector<detail::matrix_store::const_ptr> right_mats;
+	const block_matrix *block_m = dynamic_cast<const block_matrix *>(&m);
+	if (block_m == NULL)
+		right_mats.push_back(m.get_raw_store());
+	else
+		get_wider_matrices(block_m->store, right_mats);
+
+	std::vector<detail::matrix_store::const_ptr> left_mats;
+	get_wider_matrices(store, left_mats);
+
+	detail::matrix_store::ptr res;
+	assert(get_type() == m.get_type());
+	assert(get_type() == get_scalar_type<double>()
+			|| get_type() == get_scalar_type<float>());
+	res = detail::matrix_store::create(get_num_rows(), m.get_num_cols(),
+			out_layout, get_type(), -1, true);
+	// Each time we take one matrix in the right group and perform inner product
+	// with all matrices in the left group.
+	for (size_t i = 0; i < right_mats.size(); i++) {
+		dense_matrix::ptr right = dense_matrix::create(right_mats[i]);
+		std::vector<dense_matrix::ptr> tmp_mats(left_mats.size());
+		for (size_t j = 0; j < left_mats.size(); j++) {
+			dense_matrix::ptr left = dense_matrix::create(left_mats[j]);
+			tmp_mats[j] = left->multiply(*right, out_layout);
+			// This is only necessary for EM matrices.
+			if (!left->is_in_mem()) {
+				detail::matrix_store &tmp
+					= const_cast<detail::matrix_store &>(left->get_data());
+				tmp.set_cache_portion(false);
+			}
+		}
+		if (!right->is_in_mem()) {
+			detail::matrix_store &tmp
+				= const_cast<detail::matrix_store &>(right->get_data());
+			tmp.set_cache_portion(true);
+		}
+		materialize(tmp_mats, false);
+
+		// We now copy the inner product result to the final matrix.
+		size_t right_block_size = right_mats[0]->get_num_cols();
+		size_t left_block_size = left_mats[0]->get_num_rows();
+		size_t col_idx = i * right_block_size;
+		for (size_t j = 0; j < tmp_mats.size(); j++) {
+			size_t row_idx = j * left_block_size;
+			size_t num_rows = std::min(left_block_size, get_num_rows() - row_idx);
+			size_t num_cols = std::min(right_block_size,
+					m.get_num_cols() - col_idx);
+			assert(num_rows == tmp_mats[j]->get_num_rows());
+			assert(num_cols == tmp_mats[j]->get_num_cols());
+			detail::local_matrix_store::ptr res_part = res->get_portion(row_idx,
+					col_idx, num_rows, num_cols);
+			detail::local_matrix_store::const_ptr src_part
+				= tmp_mats[j]->get_data().get_portion(0);
+			res_part->copy_from(*src_part);
+		}
+	}
+	return dense_matrix::create(res);
 }
 
 dense_matrix::ptr block_matrix::multiply(const dense_matrix &mat,
