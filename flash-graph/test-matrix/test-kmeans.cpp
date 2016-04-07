29e414b62ebe76462eed439223fdbb1e1cb77e6d
Disa Mhembere <disa@jhu.edu>
2016-04-07 00:55:33 -0400

    [Test]: Adding pthread impl to regular testing suite

--- flash-graph/test-matrix/test-kmeans.cpp
+++ flash-graph/test-matrix/test-kmeans.cpp
@@ -2,6 +2,8 @@
 #include "FGlib.h"
 #include "matrix/kmeans.h"
 #include "libgraph-algs/sem_kmeans.h"
+#include "matrix/kmeans_coordinator.h"
+#include "matrix/kmeans_task_coordinator.h"
 
 using namespace fg;
 
@@ -34,13 +36,15 @@ int main(int argc, char* argv[]) {
 	int num_opts = 0;
 	double tolerance = -1;
     bool use_min_tri = false;
+    bool pthread = false;
+    unsigned nnodes = numa_num_task_nodes();
 
     // Increase by 3 -- getopt ignores argv[0]
 	argv += 3;
 	argc -= 3;
     
 	signal(SIGINT, int_handler);
-	while ((opt = getopt(argc, argv, "l:i:t:T:d:C:m")) != -1) {
+	while ((opt = getopt(argc, argv, "l:i:t:T:d:C:mpN:")) != -1) {
 		num_opts++;
 		switch (opt) {
 			case 'l':
@@ -73,6 +77,14 @@ int main(int argc, char* argv[]) {
 				use_min_tri = true;
 				num_opts++;
 				break;
+			case 'p':
+				pthread = true;
+				num_opts++;
+				break;
+			case 'N':
+				nnodes = atoi(optarg);
+				num_opts++;
+				break;
 			default:
 				print_usage();
 		}
@@ -86,33 +98,48 @@ int main(int argc, char* argv[]) {
     br.read(p_data);
     printf("Read data!\n");
 
-    double* p_centers = new double [k*ncol];
+    double* p_centers = NULL;
 
     if (is_file_exist(centersfn.c_str())) {
+        p_centers = new double [k*ncol];
         bin_reader<double> br2(centersfn, k, ncol);
         br2.read(p_centers);
         printf("Read centers!\n");
     } else
         printf("No centers to read ..\n");
-
-    unsigned* p_clust_asgns = new unsigned [nrow];
-    unsigned* p_clust_asgn_cnt = new unsigned [k];
-
-    if (use_min_tri) {
-        compute_min_kmeans(p_data, p_centers, p_clust_asgns,
-                p_clust_asgn_cnt, nrow, ncol, k, max_iters,
-                nthread, init, tolerance, dist_type);
+    if (pthread) {
+        if (use_min_tri) {
+            prune::kmeans_task_coordinator::ptr kc = prune::kmeans_task_coordinator::create(
+                    datafn, nrow, ncol, k, max_iters, nnodes, nthread, p_centers,
+                    init, tolerance, dist_type);
+            kc->run_kmeans();
+        } else {
+            kmeans_coordinator::ptr kc = kmeans_coordinator::create(datafn,
+                    nrow, ncol, k, max_iters, nnodes, nthread, p_centers,
+                    init, tolerance, dist_type);
+            kc->run_kmeans();
+        }
     } else {
-        compute_kmeans(p_data, p_centers, p_clust_asgns,
-                p_clust_asgn_cnt, nrow, ncol, k, max_iters,
-                nthread, init, tolerance, dist_type);
+        unsigned* p_clust_asgns = new unsigned [nrow];
+        unsigned* p_clust_asgn_cnt = new unsigned [k];
+        p_centers = new double [k*ncol];
+
+        if (use_min_tri) {
+            compute_min_kmeans(p_data, p_centers, p_clust_asgns,
+                    p_clust_asgn_cnt, nrow, ncol, k, max_iters,
+                    nthread, init, tolerance, dist_type);
+        } else {
+            compute_kmeans(p_data, p_centers, p_clust_asgns,
+                    p_clust_asgn_cnt, nrow, ncol, k, max_iters,
+                    nthread, init, tolerance, dist_type);
+        }
+
+        delete [] p_clust_asgns;
+        delete [] p_clust_asgn_cnt;
+        delete [] p_data;
     }
 
-    delete [] p_clust_asgns;
-    delete [] p_clust_asgn_cnt;
-
-    delete [] p_data;
-    delete [] p_centers;
+    if (p_centers) delete [] p_centers;
 
     return EXIT_SUCCESS;
 }
@@ -128,4 +155,6 @@ void print_usage() {
     fprintf(stderr, "-l tolerance for convergence (1E-6)\n");
     fprintf(stderr, "-d Distance matric [eucl,cos]\n");
     fprintf(stderr, "-m Use the minimal triangle inequality\n");
+    fprintf(stderr, "-p Use pthread routine instead of OMP\n");
+    fprintf(stderr, "-N No. of numa nodes you want to use\n");
 }
