5cfd2c62fe607fb05a976aa2f59571a227a7dd6a
Disa Mhembere <disa@jhu.edu>
2016-04-08 18:33:14 -0400

    [Admin]: namespacing move of function

--- flash-graph/libgraph-algs/dist_matrix.h
+++ flash-graph/libgraph-algs/dist_matrix.h
@@ -24,8 +24,7 @@
 #include "clusters.h"
 
 using namespace km;
-namespace
-{
+namespace prune {
     // NOTE: Creates a matrix like this e.g for K = 5
     /* - Don't store full matrix, don't store dist to myself -> space: (k*k-1)/2
        0 ==> 1 2 3 4
@@ -34,8 +33,7 @@ namespace
        3 ==> 4
        (4 ==> not needed)
        */
-    class dist_matrix
-    {
+    class dist_matrix {
         private:
             std::vector<std::vector<double>> mat;
             unsigned rows;
@@ -104,38 +102,37 @@ namespace
                     print_vector<double>(mat[row]);
                 }
             }
-    };
 
-    void compute_dist(prune_clusters::ptr cls, dist_matrix::ptr dm,
-            const unsigned ncol) {
-        if (cls->get_nclust() <= 1) return;
+            void compute_dist(prune_clusters::ptr cls, const unsigned ncol) {
+                if (cls->get_nclust() <= 1) return;
 
-        BOOST_VERIFY(dm->get_num_rows() == cls->get_nclust()-1);
-        cls->reset_s_val_v();
-        //#pragma omp parallel for collapse(2) // FIXME: Opt Coalese perhaps
-        for (unsigned i = 0; i < cls->get_nclust(); i++) {
-            for (unsigned j = i+1; j < cls->get_nclust(); j++) {
-                double dist = eucl_dist(&(cls->get_means()[i*ncol]),
-                        &(cls->get_means()[j*ncol]), ncol) / 2.0;
-                dm->set(i,j, dist);
+                BOOST_VERIFY(get_num_rows() == cls->get_nclust()-1);
+                cls->reset_s_val_v();
+                //#pragma omp parallel for collapse(2) // FIXME: Opt Coalese perhaps
+                for (unsigned i = 0; i < cls->get_nclust(); i++) {
+                    for (unsigned j = i+1; j < cls->get_nclust(); j++) {
+                        double dist = eucl_dist(&(cls->get_means()[i*ncol]),
+                                &(cls->get_means()[j*ncol]), ncol) / 2.0;
+                        set(i,j, dist);
 
-                // Set s(x) for each cluster
-                if (dist < cls->get_s_val(i)) {
-                    cls->set_s_val(dist, i);
+                        // Set s(x) for each cluster
+                        if (dist < cls->get_s_val(i)) {
+                            cls->set_s_val(dist, i);
+                        }
+
+                        if (dist < cls->get_s_val(j)) {
+                            cls->set_s_val(dist, j);
+                        }
+                    }
                 }
-
-                if (dist < cls->get_s_val(j)) {
-                    cls->set_s_val(dist, j);
-                }
-            }
-        }
 #if VERBOSE
-        for (unsigned cl = 0; cl < cls->get_nclust(); cl++) {
-            BOOST_VERIFY(cls->get_s_val(cl) == dm->get_min_dist(cl));
-            BOOST_LOG_TRIVIAL(info) << "cl:" << cl << " get_s_val: "
-                << cls->get_s_val(cl);
-        }
+                for (unsigned cl = 0; cl < cls->get_nclust(); cl++) {
+                    BOOST_VERIFY(cls->get_s_val(cl) == get_min_dist(cl));
+                    BOOST_LOG_TRIVIAL(info) << "cl:" << cl << " get_s_val: "
+                        << cls->get_s_val(cl);
+                }
 #endif
-    }
+            }
+    };
 }
 #endif
