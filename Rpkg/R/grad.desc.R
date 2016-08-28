997452fba43d79dd8a0638b0e60635c466f027fb
Da Zheng <zhengda1936@gmail.com>
2016-08-28 00:55:53 -0400

    [R]: use the regular naming convension.

--- Rpkg/R/grad.desc.R
+++ Rpkg/R/grad.desc.R
@@ -1,26 +1,26 @@
-gradient.descent <- function(x, y, get.grad, get.hessian, cost=NULL, alpha=0.1,
+gradient.descent <- function(X, y, get.grad, get.hessian, cost=NULL, alpha=0.1,
 							 num.iterations=500, output.path=FALSE)
 {
 	# Add x_0 = 1 as the first column
-	m <- if(is.vector(x)) length(x) else nrow(x)
-	if(is.vector(x) || (!all(x[,1] == 1))) x <- fm.cbind(fm.rep.int(1, m), x)
+	m <- if(is.vector(X)) length(X) else nrow(X)
+	if(is.vector(X) || (!all(X[,1] == 1))) X <- fm.cbind(fm.rep.int(1, m), X)
 
-	num.features <- ncol(x)
+	num.features <- ncol(X)
 
 	# Initialize the parameters
 	theta <- matrix(rep(0, num.features), nrow=1)
 
-	L2 <- function(x) {
-		sqrt(sum(x * x))
+	L2 <- function(X) {
+		sqrt(sum(X * X))
 	}
 
 	# Look at the values over each iteration
 	theta.path <- theta
 	for (i in 1:num.iterations) {
-		g <- get.grad(x, y, theta)
+		g <- get.grad(X, y, theta)
 		if (!is.null(get.hessian) && i > 5) {
-			D2 <- get.hessian(x, y, theta)
-			H <- fm.conv.FM2R(t(x) %*% sweep(x, 1, fm.as.vector(D2), FUN="*"))
+			D2 <- get.hessian(X, y, theta)
+			H <- fm.conv.FM2R(t(X) %*% sweep(X, 1, fm.as.vector(D2), FUN="*"))
 			H <- H / min(abs(H))
 			z <- pcg(H, as.vector(-g))
 			z <- t(z)
@@ -35,7 +35,7 @@ gradient.descent <- function(x, y, get.grad, get.hessian, cost=NULL, alpha=0.1,
 		if (is.null(cost))
 			cat(i,  ": L2(g) =", L2(g), "\n")
 		else
-			cat(i,  ": L2(g) =", L2(g), ", cost:", cost(x, y, theta), "\n")
+			cat(i,  ": L2(g) =", L2(g), ", cost:", cost(X, y, theta), "\n")
 	}
 
 	if(output.path) return(theta.path) else return(theta.path[nrow(theta.path),])
--- Rpkg/R/regression.R
+++ Rpkg/R/regression.R
@@ -1,27 +1,27 @@
-logistic.grad <- function(x, y, theta)
+logistic.grad <- function(X, y, w)
 {
-	gradient <- (1 / length(y)) * (t(x) %*% (1/(1 + exp(-x %*% t(theta))) - y))
+	gradient <- (1 / length(y)) * (t(X) %*% (1/(1 + exp(-X %*% t(w))) - y))
 	return(t(fm.conv.FM2R(gradient)))
 }
 
-logistic.hessian <- function(x, y, w)
+logistic.hessian <- function(X, y, w)
 {
-	exw <- exp(x %*% t(w))
+	exw <- exp(X %*% t(w))
 	ifelse(is.infinite(exw), 0, exw/((1+exw)^2))
 }
 
-logistic.cost <- function(x, y, theta)
+logistic.cost <- function(X, y, w)
 {
 	m <- length(y)
-	(1/m)*sum(y*(-x %*% t(theta)) + log(1 + exp(x %*% t(theta))))
+	(1/m)*sum(y*(-X %*% t(w)) + log(1 + exp(X %*% t(w))))
 }
 
-logistic.regression <- function(x, y, method=c("GD", "Newton"))
+logistic.regression <- function(X, y, method=c("GD", "Newton"))
 {
 	if (method == "GD")
 		get.hessian <- NULL
 	else
 		get.hessian <- logistic.hessian
-	gradient.descent(x, y, logistic.grad, get.hessian,
+	gradient.descent(X, y, logistic.grad, get.hessian,
 					 cost=logistic.cost, alpha=0.00000001)
 }
