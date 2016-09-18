7e3f16e87ea50af52f8856022ae647ea48ab6a81
Da Zheng <zhengda1936@gmail.com>
2016-09-17 22:31:38 -0400

    [R]: add QOQ.

--- Rpkg/R/LOL.R
+++ Rpkg/R/LOL.R
@@ -55,3 +55,42 @@ LOL <- function(m, labels, k, type=c("svd", "rand_dense", "rand_sparse")) {
 		NULL
 	}
 }
+
+QOQ <- function(m, labels, k)
+{
+	rlabels <- fm.conv.FM2R(labels)
+	# we have to make sure they are binary labels and their values
+	# are 0 or 1.
+	stopifnot(sum(rlabels == 0 | rlabels == 1) == length(rlabels))
+
+	counts <- as.data.frame(table(rlabels))
+	print(counts)
+	num.labels <- length(counts$Freq)
+	num.features <- dim(m)[1]
+	nv <- k - (num.labels - 1)
+	nv0 <- min(nv, counts$Freq[1])
+	nv1 <- min(nv, counts$Freq[2])
+
+	gr.sum <- fm.groupby(m, 1, fm.as.factor(labels, 2), fm.bo.add)
+	gr.mean <- fm.mapply.row(gr.sum, counts$Freq, fm.bo.div, FALSE)
+	diff <- fm.get.cols(gr.mean, 1) - fm.get.cols(gr.mean, 2)
+	diff.l2 <- sqrt(sum(diff * diff))
+	stopifnot(diff.l2 != 0)
+	diff <- diff / diff.l2
+	if (nv == 0)
+		return(diff)
+
+	c0.idxs <- which(rlabels == 0)
+	c1.idxs <- which(rlabels == 1)
+	m0 <- sweep(m[, c0.idxs], 1, gr.mean[,1], "-")
+	m1 <- sweep(m[, c1.idxs], 1, gr.mean[,2], "-")
+	svd0 <- fm.svd(m0, nv=nv0, nu=nv0)
+	svd1 <- fm.svd(m1, nv=nv1, nu=nv1)
+	vals <- c(svd0$d, svd1$d)
+	vecs <- fm.cbind(diff, svd0$u, svd1$u)
+	sort.res <- sort(vals, decreasing=TRUE, index.return=TRUE)
+	print(sort.res$x)
+	print(sort.res$ix)
+	col.idxs <- c(1, sort.res$ix + 1)
+	vecs[,col.idxs]
+}
