01c71d5411c3c9cd00c4a72861f8ccb6db5cdac4
Da Zheng <zhengda1936@gmail.com>
2016-11-28 18:36:17 -0500

    [Matrix]: update unit tests on sink matrices.

--- matrix/unit-test/test-sink_matrix.cpp
+++ matrix/unit-test/test-sink_matrix.cpp
@@ -23,17 +23,33 @@ void test_transpose()
 	agg_operate::const_ptr agg_op = agg_operate::create(add, add);
 	sink_store::const_ptr sink;
 	matrix_store::const_ptr t;
+	std::vector<matrix_store::const_ptr> sinks;
 
+	// Agg matrix.
 	sink = sink_store::const_ptr(new agg_matrix_store(data,
 				matrix_margin::MAR_COL, agg_op));
 	printf("agg sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
-	assert(sink->get_num_rows() == data->get_num_cols());
-	assert(sink->get_num_cols() == 1);
+	assert(sink->get_num_rows() == 1);
+	assert(sink->get_num_cols() == data->get_num_cols());
 	t = sink->transpose();
 	printf("t sink: %ld, %ld\n", t->get_num_rows(), t->get_num_cols());
 	assert(t->get_num_rows() == data->get_num_cols());
 	assert(t->get_num_cols() == 1);
 
+	// Block agg matrix.
+	sinks.resize(3);
+	for (size_t i = 0; i < sinks.size(); i++)
+		sinks[i] = sink;
+	sink = block_sink_store::create(sinks, 1, sinks.size());
+	printf("block sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
+	assert(sink->get_num_rows() == 1);
+	assert(sink->get_num_cols() == sinks.size() * data->get_num_cols());
+	t = sink->transpose();
+	printf("t sink: %ld, %ld\n", t->get_num_rows(), t->get_num_cols());
+	assert(t->get_num_rows() == sinks.size() * data->get_num_cols());
+	assert(t->get_num_cols() == 1);
+
+	// IPW matrix.
 	sink = sink_store::const_ptr(new IPW_matrix_store(data->transpose(), data2,
 				multiply, add));
 	printf("IPW sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
@@ -44,22 +60,10 @@ void test_transpose()
 	assert(t->get_num_rows() == data2->get_num_cols());
 	assert(t->get_num_cols() == data->get_num_cols());
 
-	factor_col_vector::const_ptr labels = factor_col_vector::create(factor(10),
-			dense_matrix::create_randu<int>(0, 10, data->get_num_rows(), 1,
-				matrix_layout_t::L_COL));
-	sink = sink_store::const_ptr(new groupby_matrix_store(data,
-				labels, matrix_margin::MAR_ROW, agg_op));
-	printf("groupby sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
-	assert(sink->get_num_rows() == 10);
-	assert(sink->get_num_cols() == data->get_num_cols());
-	t = sink->transpose();
-	printf("t sink: %ld, %ld\n", t->get_num_rows(), t->get_num_cols());
-	assert(t->get_num_rows() == data->get_num_cols());
-	assert(t->get_num_cols() == 10);
-
+	// Block IPW matrix.
 	size_t num_block_rows = 3;
 	size_t num_block_cols = 2;
-	std::vector<matrix_store::const_ptr> sinks(num_block_rows * num_block_cols);
+	sinks.resize(num_block_rows * num_block_cols);
 	for (size_t i = 0; i < num_block_rows; i++) {
 		for (size_t j = 0; j < num_block_cols; j++) {
 			matrix_store::const_ptr right = j == num_block_cols - 1 ? data3 : data2;
@@ -77,6 +81,33 @@ void test_transpose()
 	assert(t->get_num_rows() == (num_block_cols - 1) * data2->get_num_cols()
 			+ data3->get_num_cols());
 	assert(t->get_num_cols() == data->get_num_cols() * num_block_rows);
+
+	// Groupby matrix
+	factor_col_vector::const_ptr labels = factor_col_vector::create(factor(10),
+			dense_matrix::create_randu<int>(0, 10, data->get_num_rows(), 1,
+				matrix_layout_t::L_COL));
+	sink = sink_store::const_ptr(new groupby_matrix_store(data,
+				labels, matrix_margin::MAR_ROW, agg_op));
+	printf("groupby sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
+	assert(sink->get_num_rows() == 10);
+	assert(sink->get_num_cols() == data->get_num_cols());
+	t = sink->transpose();
+	printf("t sink: %ld, %ld\n", t->get_num_rows(), t->get_num_cols());
+	assert(t->get_num_rows() == data->get_num_cols());
+	assert(t->get_num_cols() == 10);
+
+	// Block groupby matrix.
+	sinks.resize(3);
+	for (size_t i = 0; i < sinks.size(); i++)
+		sinks[i] = sink;
+	sink = block_sink_store::create(sinks, 1, sinks.size());
+	printf("block sink: %ld, %ld\n", sink->get_num_rows(), sink->get_num_cols());
+	assert(sink->get_num_rows() == 10);
+	assert(sink->get_num_cols() == sinks.size() * data->get_num_cols());
+	t = sink->transpose();
+	printf("t sink: %ld, %ld\n", t->get_num_rows(), t->get_num_cols());
+	assert(t->get_num_rows() == sinks.size() * data->get_num_cols());
+	assert(t->get_num_cols() == 10);
 }
 
 int main(int argc, char *argv[])
