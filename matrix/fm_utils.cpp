e8c4c8084e98fd70172d24eaf9a1b37a77740457
Da Zheng <zhengda1936@gmail.com>
2017-01-07 21:56:24 +0800

    [Matrix]: fix a bug in loading a symmetric sparse matrix.

--- matrix/fm_utils.cpp
+++ matrix/fm_utils.cpp
@@ -709,18 +709,46 @@ std::shared_ptr<sparse_matrix> create_2d_matrix(data_frame::ptr df,
 	}
 	assert(seq_vec->get_length() == rep_vec->get_length());
 
-	// I artificially add an invalid out-edge for each vertex, so it's
-	// guaranteed that each vertex exists in the adjacency lists.
-	data_frame::ptr new_df = data_frame::create();
-	new_df->add_vec(df->get_vec_name(0), seq_vec);
-	new_df->add_vec(df->get_vec_name(1), rep_vec);
-	if (df->get_num_vecs() == 3) {
-		assert(attr_extra);
-		new_df->add_vec(df->get_vec_name(2), attr_extra);
-	}
-	df->append(new_df);
+	if (is_sym) {
+		// I artificially add an invalid out-edge for each vertex, so it's
+		// guaranteed that each vertex exists in the adjacency lists.
+		data_frame::ptr new_df = data_frame::create();
+		new_df->add_vec(df->get_vec_name(0), seq_vec);
+		new_df->add_vec(df->get_vec_name(1), rep_vec);
+		if (df->get_num_vecs() == 3) {
+			assert(attr_vec);
+			new_df->add_vec(df->get_vec_name(2), attr_vec);
+		}
+		df->append(new_df);
 
-	if (!is_sym) {
+		detail::vec_store::ptr vec0 = df->get_vec(0)->deep_copy();
+		detail::vec_store::ptr vec1 = df->get_vec(1)->deep_copy();
+		detail::vec_store::ptr vec2;
+		if (df->get_num_vecs() == 3)
+			vec2 = df->get_vec(2)->deep_copy();
+		vec0->append(df->get_vec_ref(1));
+		vec1->append(df->get_vec_ref(0));
+		if (df->get_num_vecs() == 3)
+			vec2->append(df->get_vec_ref(2));
+		new_df = data_frame::create();
+		new_df->add_vec(df->get_vec_name(0), vec0);
+		new_df->add_vec(df->get_vec_name(1), vec1);
+		if (df->get_num_vecs() == 3)
+			new_df->add_vec(df->get_vec_name(2), vec2);
+		df = new_df;
+	}
+	else {
+		// I artificially add an invalid out-edge for each vertex, so it's
+		// guaranteed that each vertex exists in the adjacency lists.
+		data_frame::ptr new_df = data_frame::create();
+		new_df->add_vec(df->get_vec_name(0), seq_vec);
+		new_df->add_vec(df->get_vec_name(1), rep_vec);
+		if (df->get_num_vecs() == 3) {
+			assert(attr_extra);
+			new_df->add_vec(df->get_vec_name(2), attr_extra);
+		}
+		df->append(new_df);
+
 		// I artificially add an invalid in-edge for each vertex.
 		new_df = data_frame::create();
 		new_df->add_vec(df->get_vec_name(1), seq_vec);
