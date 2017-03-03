ab3e4afabde73342da5fe3db1c3a2ba246ca8fd4
Da Zheng <zhengda1936@gmail.com>
2017-03-02 23:29:06 -0500

    [R]: move unit test of FlashGraphR again.

--- FlashGraphR/inst/tests/verify.vs.igraph.R
+++ FlashGraphR/inst/tests/verify.vs.igraph.R
@@ -1,5 +1,4 @@
-require(igraph)
-library("FlashR")
+library("FlashGraphR")
 
 ig.local.scan <- function(g, order)
 {
@@ -17,12 +16,13 @@ ig.local.scan <- function(g, order)
 
 verify.cc <- function(fg.res, ig.res)
 {
+	fg.res <- as.vector(fg.res)
 	# get unique components IDs
 	uniq.fg.res <- unique(fg.res)
 	uniq.ig.res <- unique(ig.res)
 
 	# get the number of isolates from the FlashGraph results.
-	fg.no.iso <- sum(fg.res == -1)
+	fg.no.iso <- sum(fg.res == 2^32-1)
 
 	# get the number of unique component IDs
 	ig.uniq.no <- length(uniq.ig.res)
@@ -32,7 +32,7 @@ verify.cc <- function(fg.res, ig.res)
 	} else {
 		fg.uniq.no <- length(uniq.fg.res) - 1 + fg.no.iso
 	}
-	stopifnot(fg.uniq.no == ig.uniq.no)
+	expect_equal(fg.uniq.no, ig.uniq.no)
 
 	# counts the size of each components from FlashGraph
 	fg.res.counts <- table(fg.res)
@@ -44,21 +44,17 @@ verify.cc <- function(fg.res, ig.res)
 		# exclude the isolates from FlashGraph
 		fg.uniq.no <- length(fg.res.counts) - 1
 		# get the size of each components from FlashGraph, excluding isolates
-		fg.sorted <- sort(fg.res.counts[(1:fg.uniq.no) + 1])
+		fg.sorted <- sort(fg.res.counts[1:fg.uniq.no])
 	}
 	# get the size of each components from iGraph, excluding isolates
 	ig.sorted <- sort(table(ig.res))[(ig.uniq.no - fg.uniq.no + 1):ig.uniq.no]
 	cmp.res <- fg.sorted == ig.sorted
-	stopifnot(sum(cmp.res) == length(cmp.res))
+	expect_equal(sum(cmp.res), length(cmp.res))
 }
 
 check.vectors <- function(name, fg.res, ig.res)
 {
-	cmp.res <- fg.res == ig.res
-	if (sum(cmp.res) != length(cmp.res)) {
-		cat(name, "doesn't pass the test\n")
-		stopifnot(FALSE)
-	}
+	expect_equal(as.vector(fg.res), ig.res)
 }
 
 check.matrices <- function(name, fg.res, ig.res)
@@ -104,7 +100,8 @@ test.directed <- function(fg, ig)
 	fg.res <- fg.page.rank(fg)
 	ig.res <- page.rank.old(ig, eps=0.01, old=TRUE)
 	num <- sum((abs(fg.res - ig.res) / abs(fg.res)) < 0.02)
-	cat("# vertices whose PR diff <= 2% is", num, ", # vertices:", vcount(ig), "\n")
+	cat("# vertices whose PR diff <= 2% is", as.vector(num),
+		", # vertices:", vcount(ig), "\n")
 
 	# test locality scan
 	print("test locality statistics")
@@ -120,30 +117,6 @@ test.directed <- function(fg, ig)
 	fg.res <- fg.topK.scan(fg, K=10)
 	ig.res <- sort(ig.res, decreasing=TRUE)[1:10]
 	check.vectors("topK-scan_test", fg.res$scan, ig.res)
-
-	# test matrix multiplication.
-	print("A * x");
-	x <- runif(vcount(ig), 0, 1)
-	ig.matrix <- get.adjacency(ig)
-	fg.res <- fg.multiply(fg, x)
-	ig.res <- ig.matrix %*% x
-	check.vectors("A * x", fg.res, ig.res)
-
-	print("t(A) * x");
-	fg.res <- fg.multiply(fg, x, TRUE)
-	ig.res <- t(as.matrix(ig.matrix)) %*% x
-	check.vectors("t(A) * x", fg.res, ig.res)
-
-	print("A * m");
-	x <- matrix(runif(vcount(ig) * 5, 0, 1), nrow=vcount(ig), ncol=5)
-	fg.res <- fg.multiply.matrix(fg, x)
-	ig.res <- ig.matrix %*% x
-	check.matrices("A * m", fg.res, ig.res)
-
-	print("t(A) * m")
-	fg.res <- fg.multiply.matrix(fg, x, TRUE)
-	ig.res <- t(as.matrix(ig.matrix)) %*% x
-	check.matrices("t(A) * m", fg.res, ig.res)
 }
 
 test.undirected <- function(fg, ig)
@@ -170,49 +143,22 @@ test.undirected <- function(fg, ig)
 	print("test local transitivity")
 	fg.res <- fg.transitivity(fg, type="local")
 	ig.res <- transitivity(ig, type="local")
-	fg.res[is.nan(fg.res)] <- 0
+	fg.res <- ifelse(is.nan(fg.res), 0, fg.res)
 	ig.res[is.nan(ig.res)] <- 0
 	check.vectors("local transitivity", fg.res, ig.res)
 
 	print("test global transitivity")
 	fg.res <- fg.transitivity(fg, type="global")
 	ig.res <- transitivity(ig, type="global")
-	stopifnot(fg.res == ig.res)
-
-	# test matrix multiplication.
-	print("A * x");
-	x <- runif(vcount(ig), 0, 1)
-	ig.matrix <- get.adjacency(ig)
-	fg.res <- fg.multiply(fg, x)
-	ig.res <- ig.matrix %*% x
-	check.vectors("A * x", fg.res, ig.res)
-
-	print("t(A) * x");
-	fg.res <- fg.multiply(fg, x, TRUE)
-	check.vectors("t(A) * x", fg.res, ig.res)
-
-	print("A * m");
-	x <- matrix(runif(vcount(ig) * 5, 0, 1), nrow=vcount(ig), ncol=5)
-	fg.res <- fg.multiply.matrix(fg, x)
-	ig.res <- ig.matrix %*% x
-	check.matrices("A * m", fg.res, ig.res)
-
-	print("t(A) * m")
-	fg.res <- fg.multiply.matrix(fg, x, TRUE)
-	check.matrices("t(A) * m", fg.res, ig.res)
+	expect_equal(as.vector(fg.res), ig.res)
 }
-
-# Kmeans
-source("verify.kmeans.R")
-test.kmeans(2, 500, 20)
-test.with.iris()
 
 # Betweeness
 test.betweenness <- function(fg, ig)
 {
 	fg.btw <- fg.betweenness(fg)
 	ig.btw <- betweenness(ig)
-	stopifnot(all.equal(ig.btw, fg.btw, tolerance=.001)) # Need tolerance for floats
+	expect_true(all.equal(ig.btw, fg.btw, tolerance=.001)) # Need tolerance for floats
 }
 
 test.weighted <- function(fg, ig)
@@ -220,22 +166,15 @@ test.weighted <- function(fg, ig)
 	fg.out <- fg.multiply(fg, rep.int(1, fg.vcount(fg)))
 	adj <- get.adjacency(ig, attr='V3', type="both")
 	ig.out <- adj %*% rep.int(1, vcount(ig))
-	stopifnot(sum(fg.out) == sum(ig.out))
+	expect_that(as.vector(fg.out), ig.out)
 }
 
 # Test on a directed graph.
+download.file("http://snap.stanford.edu/data/wiki-Vote.txt.gz", "wiki-Vote.txt.gz")
+system("gunzip wiki-Vote.txt.gz")
+system("sed '/^#/d' wiki-Vote.txt > wiki-Vote1.txt")
 ig <- read.graph("wiki-Vote1.txt")
-
-fg <- fg.load.graph("wiki-Vote.adj", index="wiki-Vote.index", graph.name="wiki")
-
-print("run in the standalone mode")
-print("load a graph in adjacency list")
-fg <- fg.load.graph("wiki-Vote.adj", index="wiki-Vote.index", graph.name="wiki")
-test.directed(fg, ig)
-
-cat("\n\n\n")
-print("load a graph in edge lists")
-fg <- fg.load.graph("wiki-Vote1.txt", graph.name="wiki")
+fg <- fg.load.graph("wiki-Vote.txt", graph.name="wiki")
 test.directed(fg, ig)
 fg.list.graphs()
 
@@ -246,37 +185,31 @@ test.directed(fg, ig)
 fg.list.graphs()
 
 #cat("\n\n\n")
-print("run in the SAFS mode")
-fg.set.conf("run_test.txt")
-fg <- fg.get.graph("wiki-Vote")
-test.directed(fg, ig)
-fg.list.graphs()
+#print("run in the SAFS mode")
+#fg.set.conf("run_test.txt")
+#fg <- fg.get.graph("wiki-Vote")
+#test.directed(fg, ig)
+#fg.list.graphs()
+file.remove("wiki-Vote.txt")
+file.remove("wiki-Vote1.txt")
 
 # Now test on an undirected graph
+download.file("http://snap.stanford.edu/data/facebook_combined.txt.gz", "facebook_combined.txt.gz")
+system("gunzip facebook_combined.txt.gz")
+system("sed '/^#/d' facebook_combined.txt > facebook_combined1.txt")
 ig <- read.graph("facebook_combined1.txt", directed=FALSE)
-
-fg <- fg.load.graph("facebook.adj", index="facebook.index", graph.name="facebook")
+fg <- fg.load.graph("facebook_combined.txt", directed=FALSE, graph.name="facebook")
 
 print("load a graph in adjacency list")
-fg <- fg.load.graph("facebook.adj", index="facebook.index", graph.name="facebook")
 test.undirected(fg, ig)
-
-cat("\n\n\n")
-print("load a graph in edge lists")
-fg <- fg.load.graph("facebook_combined1.txt", directed=FALSE, graph.name="facebook")
-test.undirected(fg, ig)
-fg.list.graphs()
 
 cat("\n\n\n")
 print("load a graph from igraph")
 fg <- fg.load.igraph(ig, graph.name="facebook")
 test.undirected(fg, ig)
 fg.list.graphs()
-
-cat("\n\n\n")
-fg <- fg.get.graph("facebook")
-test.undirected(fg, ig)
-fg.list.graphs()
+file.remove("facebook_combined.txt")
+file.remove("facebook_combined1.txt")
 
 # Now test on a weighted undirected graph
 #print("load a weighted graph")
