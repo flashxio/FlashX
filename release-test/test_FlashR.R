ffd483bfe04bb930ec3675f1338bab6a3bb0b878
Da Zheng <zhengda1936@gmail.com>
2017-04-05 22:10:23 -0400

    return non-zero to shell if test on FlashR fails.

--- release-test/test_FlashR.R
+++ release-test/test_FlashR.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(FlashR)
 
-test_file("Rpkg/inst/tests/test_dense.R")
+ret <- test_file("Rpkg/inst/tests/test_dense.R")
+msgs <- capture.output(ret)
+tests <- length(msgs) / 3
+fails <- sum(as.integer(sapply(strsplit(msgs[(tests+2):(2*tests)], " +"),
+					  function(x) x[length(x)-1])))
+q(status=fails)
