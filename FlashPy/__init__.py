6c415443a0b3b248a0b4e185266740689b8857f0
Da Zheng <zhengda1936@gmail.com>
2017-07-12 14:04:23 -0400

    [FlashPy]: import submodules of flashpy by default.

---
+++ FlashPy/__init__.py
@@ -0,0 +1,19 @@
+from mat import array
+from mat import empty_like
+from mat import empty
+from mat import ones
+from mat import zeros
+from mat import arange
+from mat import sum
+from mat import prod
+from mat import mean
+from mat import average
+from mat import dot
+from mat import sqrt
+from mat import absolute
+from mat import where
+from mat import maximum
+from mat import minimum
+from mat import init_flashpy
+import linalg
+import sparse
---
+++ FlashPy/linalg/__init__.py
@@ -0,0 +1,1 @@
+from svd import svds
