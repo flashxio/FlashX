095f067deb59d00e071fae12ad2b409781f2c293
Da Zheng <zhengda1936@gmail.com>
2017-08-07 20:28:09 -0400

    [FlashPy]: import LU from SciPy.

--- FlashPy/linalg/__init__.py
+++ FlashPy/linalg/__init__.py
@@ -3,3 +3,6 @@ from qr import qr
 from norm import norm
 
 from numpy.linalg import cholesky, eig, svd
+
+# TODO we should implement LU instead of simply importing it from SciPy.
+from scipy.linalg import lu
