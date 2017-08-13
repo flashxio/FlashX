1289ac2c508ac67a354ccd1c3709c1b1a695cf8c
Da Zheng <zhengda1936@gmail.com>
2017-08-13 17:21:36 -0400

    Remove the scripts for release tests.

--- release-test/install_Rpkgs.R
+++
@@ -1,6 +0,0 @@
-r <- getOption("repos")
-r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
-options(repos=r)
-install.packages("testthat")
-install.packages("Rcpp")
-install.packages("RSpectra")
--- release-test/test_FlashR.R
+++
@@ -1,9 +0,0 @@
-library(testthat)
-library(FlashR)
-
-ret <- test_file("Rpkg/inst/tests/test_dense.R")
-msgs <- capture.output(ret)
-tests <- length(msgs) / 3
-fails <- sum(as.integer(sapply(strsplit(msgs[(tests+2):(2*tests)], " +"),
-					  function(x) x[length(x)-1])))
-q(status=fails)
